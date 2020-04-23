6a13fbd3330c238eaa9544b032ad2abba9964ca3
donle <donle1988@gmail.com>
2020-04-23 18:22:38 +1000

    fixup the UI of asking for peach

--- src/core/skills/characters/standard/jijiu.ts
+++ src/core/skills/characters/standard/jijiu.ts
@@ -1,12 +1,10 @@
 import { VirtualCard } from 'core/cards/card';
-import { CardMatcher } from 'core/cards/libs/card_matcher';
 import { CardId } from 'core/cards/libs/card_props';
 import { Peach } from 'core/cards/standard/peach';
 import { Sanguosha } from 'core/game/engine';
 import { Player } from 'core/player/player';
 import { Room } from 'core/room/room';
 import { CommonSkill, ViewAsSkill } from 'core/skills/skill';
-import { SkillLoader } from 'core/game/package_loader/loader.skills';
 
 @CommonSkill({ name: 'jijiu', description: 'jijiu_description' })
 export class JiJiu extends ViewAsSkill {
@@ -15,8 +13,7 @@ export class JiJiu extends ViewAsSkill {
   }
 
   public canUse(room: Room, owner: Player) {
-    const peachSkill = SkillLoader.getInstance().getSkillByName('peach');
-    return true /*room.CurrentPlayer !== owner && */ /*peachSkill.canUse(room, owner)*/;
+    return room.CurrentPlayer !== owner;
   }
 
   public cardFilter(room: Room, owner: Player, cards: CardId[]): boolean {
