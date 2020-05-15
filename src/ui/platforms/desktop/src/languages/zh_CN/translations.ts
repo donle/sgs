12de88c108defc40ac4ce2d295d56569477fa132
Ho-spair <linyuy@163.com>
2020-05-15 19:51:17 +0800

    Update translations & progress

--- src/ui/platforms/desktop/src/languages/zh_CN/translations.ts
+++ src/ui/platforms/desktop/src/languages/zh_CN/translations.ts
@@ -220,6 +220,77 @@ export const characterDictionary: Word[] = [
   { source: 'luanji', target: '乱击' },
   { source: 'xueyi', target: '血裔' },
 
+  { source: 'caopi', target: '曹丕' },
+  { source: 'xingshang', target: '行殇' },
+  { source: 'fangzhu', target: '放逐' },
+  { source: 'songwei', target: '颂威' },
+
+  { source: 'xuhuang', target: '徐晃' },
+  { source: 'duanliang', target: '断粮' },
+  { source: 'jiezi', target: '截辎' },
+
+  { source: 'menghuo', target: '孟获' },
+  { source: 'huoshou', target: '祸首' },
+  { source: 'zaiqi', target: '再起' },
+
+  { source: 'zhurong', target: '祝融' },
+  { source: 'juxiang', target: '巨象' },
+  { source: 'lieren', target: '烈刃' },
+
+  { source: 'lusu', target: '鲁肃' },
+  { source: 'haoshi', target: '好施' },
+  { source: 'dimeng', target: '缔盟' },
+
+  { source: 'sunjian', target: '孙坚' },
+  { source: 'yinghun', target: '英魂' },
+  { source: 'polu', target: '破虏' },
+
+  { source: 'dongzhuo', target: '董卓' },
+  { source: 'jiuchi', target: '酒池' },
+  { source: 'roulin', target: '肉林' },
+  { source: 'benghuai', target: '崩坏' },
+  { source: 'baonve', target: '暴虐' },
+
+  { source: 'jiaxu', target: '贾诩' },
+  { source: 'wansha', target: '完杀' },
+  { source: 'luanwu', target: '乱武' },
+  { source: 'weimu', target: '帷幕' },
+
+  { source: 'zhanghe', target: '张郃' },
+  { source: 'qiaobian', target: '巧变' },
+
+  { source: 'dengai', target: '邓艾' },
+  { source: 'tuntian', target: '屯田' },
+  { source: 'zaoxian', target: '凿险' },
+  { source: 'qixi', target: '奇袭' },
+
+  { source: 'jiangwei', target: '姜维' },
+  { source: 'tiaoxin', target: '挑衅' },
+  { source: 'zhiji', target: '志继' },
+
+  { source: 'liushan', target: '刘禅' },
+  { source: 'xiangle', target: '享乐' },
+  { source: 'fangquan', target: '放权' },
+  { source: 'ruoyu', target: '若愚' },
+  { source: 'sishu', target: '思蜀' },
+
+  { source: 'sunce', target: '孙策' },
+  { source: 'jiang', target: '激昂' },
+  { source: 'hunzi', target: '魂姿' },
+  { source: 'zhiba', target: '制霸' },
+
+  { source: 'zhangzhaozhanghong', target: '张昭张纮' },
+  { source: 'zhijian', target: '直谏' },
+  { source: 'guzheng', target: '固政' },
+
+  { source: 'zuoci', target: '左慈' },
+  { source: 'huashen', target: '化身' },
+  { source: 'xinsheng', target: '新生' },
+
+  { source: 'caiwenji', target: '蔡文姬' },
+  { source: 'beige', target: '悲歌' },
+  { source: 'duanchang', target: '断肠' },
+
   { source: 'god_caocao', target: '神曹操' },
   { source: 'guixin', target: '归心' },
   { source: 'feiying', target: '飞影' },
@@ -444,14 +515,14 @@ export const skillDescriptions: Word[] = [
   {
     source: 'qimou_description',
     target:
-      '<b>限定技</b>，出牌阶段，你可以失去至少1点体力，然后直到回合结束，你计算与其他角色的距离-X，且你使用【杀】的次数上限+X（X为你以此法失去的体力值）。',
+      '<b>限定技</b>，出牌阶段，你可以失去至少1点体力并摸X张牌，然后直到回合结束，你计算与其他角色的距离-X，且你使用【杀】的次数上限+X（X为你以此法失去的体力值）。',
   },
   {
     source: 'tianxiang_description',
     target:
       '当你受到伤害时，你可以弃置一张红桃手牌，防止此伤害并选择一名其他角色，然后你选择一项：1.令其受到1点伤害，然后摸X张牌（X为其已损失体力值且至多为5）；2.令其失去1点体力，然后其获得你弃置的牌。',
   },
-  { source: 'hongyan_description', target: '<b>锁定技</b>，你的黑桃牌或你的黑桃判定牌的花色视为红桃。' },
+  { source: 'hongyan_description', target: '<b>锁定技</b>，你的黑桃牌或你的黑桃判定牌的花色视为红桃；当你于回合外失去红桃牌后，你摸一张牌。' },
   {
     source: 'buqu_description',
     target:
@@ -531,8 +602,183 @@ export const skillDescriptions: Word[] = [
   {
     source: 'xueyi_description',
     target:
-      '<b>主公技</b>，游戏开始时，你获得X枚“裔”标记（X为群雄势力角色数）；回合开始时，你可以移除一枚"裔"并摸一张牌；你每有一枚"裔"，手牌上限便+2。',
+      '<b>主公技</b>，游戏开始时，你获得X枚“裔”标记（X为群势力角色数）；回合开始时，你可以移除一枚"裔"并摸一张牌；你每有一枚"裔"，手牌上限便+2。',
   },
+  { source: 'xingshang_description', target: '当其他角色死亡时，你可以选择一项：1.获得其所有牌；2.回复1点体力。' },
+  {
+    source: 'fangzhu_description',
+    target:
+      '当你受到伤害后，你可以令伤害来源选择一项：1.摸X张牌并翻面；2.弃置X张牌并失去1点体力（X为你已损失的体力值）。',
+  },
+  {
+    source: 'songwei_description',
+    target:
+      '<b>主公技</b>，当其他魏势力的黑色判定牌生效后，其可以令你摸一张牌。',
+  },
+  {
+    source: 'duanliang_description',
+    target:
+      '你可以将一张黑色基本或装备牌当【兵粮寸断】使用；你对手牌数不小于你的角色使用【兵粮寸断】无距离限制。',
+  },
+  { source: 'jiezi_description', target: '<b>锁定技</b>，当其他角色跳过摸牌阶段后，你摸一张牌。' },
+  {
+    source: 'huoshou_description',
+    target:
+      '<b>锁定技</b>，【南蛮入侵】对你无效；当其他角色使用【南蛮入侵】指定第一个目标后，你代替其成为此牌造成伤害的伤害来源。',
+  },
+  {
+    source: 'zaiqi_description',
+    target:
+      '弃牌阶段结束时，你可以令一至X名角色选择一项：1.令你回复1点体力；2.摸一张牌（X为本回合内进入弃牌堆的红色牌数）。',
+  },
+  {
+    source: 'juxiang_description',
+    target:
+      '<b>锁定技</b>，【南蛮入侵】对你无效；当其他角色使用的【南蛮入侵】结算结束后，你获得之。',
+  },
+  {
+    source: 'lieren_description',
+    target:
+      '当你使用【杀】指定目标后，你可以与目标角色拼点，若你：赢，你获得其一张牌；没赢，你与其交换拼点牌。',
+  },
+  {
+    source: 'haoshi_description',
+    target:
+      '摸牌阶段，你可以多摸两张牌。若如此做，此阶段结束时，若你的手牌数大于5，你将一半的手牌交给除你外手牌数最少的一名角色（向下取整）。',
+  },
+  {
+    source: 'dimeng_description',
+    target:
+      '出牌阶段限一次，你可以选择两名其他角色（至少一名角色有手牌）并弃置X张牌（X为这两名角色的手牌数之差），然后令这两名角色交换其手牌。',
+  },
+  {
+    source: 'yinghun_description',
+    target:
+      '准备阶段开始时，若你已受伤，你可以选择一名其他角色并选择一项：1.令其摸X张牌并弃置一张牌；2.令其摸一张牌并弃置X张牌（X为你已损失的体力值）。',
+  },
+  {
+    source: 'polu_description',
+    target:
+      '当你杀死一名其他角色后或你死亡时，你可以令至少一名角色各摸X张牌（X为你本局游戏发动过此技能的次数+1）。',
+  },
+  {
+    source: 'jiuchi_description',
+    target:
+      '你可以将一张黑桃手牌当【酒】使用；你使用【酒】【杀】造成伤害后，本回合内你的“崩坏”无效。',
+  },
+  {
+    source: 'roulin_description',
+    target:
+      '<b>锁定技</b>，当你使用【杀】指定女性角色为目标/女性角色使用【杀】指定你为目标后，其/你需依次使用两张【闪】才能抵消此【杀】。',
+  },
+  {
+    source: 'benghuai_description',
+    target:
+      '<b>锁定技</b>，结束阶段开始时，若你不为体力值最小的角色，你选择一项：1.失去1点体力；2.减1点体力上限。',
+  },
+  {
+    source: 'baonve_description',
+    target:
+      '<b>主公技</b>，当其他群势力角色造成伤害后，其可以令你进行判定，若结果为黑桃，你回复1点体力。',
+  },
+  {
+    source: 'wansha_description',
+    target:
+      '<b>锁定技</b>，你的回合内，除处于濒死状态的角色外的其他角色不能使用【桃】。',
+  },
+  {
+    source: 'luanwu_description',
+    target:
+      '<b>限定技</b>，出牌阶段，你可以令所有其他角色选择一项：1.对除其外与其距离最近的角色使用一张【杀】；2.失去1点体力。',
+  },
+  {
+    source: 'weimu_description',
+    target:
+      '<b>锁定技</b>，你不能成为黑色锦囊牌的目标。',
+  },
+  {
+    source: 'tuntian_description',
+    target:
+      '当你于回合外失去牌后，你可以进行判定，若结果为红桃，你获得此判定牌，否则你将此判定牌置于你的武将牌上，称为“田”；你与其他角色计算距离-X（X为“田”的数量）。',
+  },
+  {
+    source: 'zaoxian_description',
+    target:
+      '<b>觉醒技</b>，准备阶段开始时，若你有不少于三张“田”，你减一点体力上限并获得“急袭”。',
+  },
+  { source: 'jixi_description', target: '你可以将一张“田”当【顺手牵羊】使用。' },
+  {
+    source: 'qiaobian_description',
+    target:
+      '你可以弃置一张手牌并跳过一个阶段（准备阶段和结束阶段除外），若你以此法跳过：摸牌阶段，你可以获得一至两名其他角色的各一张手牌；出牌阶段，你可以移动场上一张牌。',
+  },
+  {
+    source: 'tiaoxin_description',
+    target:
+      '出牌阶段限一次，你可以令一名其他角色选择一项：1.对包括你在内的角色使用一张【杀】；2.令你弃置其一张牌。',
+  },
+  {
+    source: 'zhiji_description',
+    target:
+      '<b>觉醒技</b>，准备阶段开始时，若你没有手牌，你选择一项：1.摸两张牌；2.回复1点体力，然后你减一点体力上限并获得“观星”。',
+  },
+  {
+    source: 'xiangle_description',
+    target:
+      '<b>锁定技</b>，当你成为【杀】的目标后，你令使用者选择一项：1.弃置一张基本牌；2.此【杀】对你无效。',
+  },
+  {
+    source: 'fangquan_description',
+    target:
+      '你可以跳过出牌阶段，然后于此回合的弃牌阶段开始时，弃置一张手牌并选择一名其他角色，令其于此回合结束时执行一个额外的回合。',
+  },
+  {
+    source: 'ruoyu_description',
+    target:
+      '<b>觉醒技</b>，准备阶段开始时，若你为体力值最小的角色，你增加1点体力上限并回复1点体力，然后获得“激将”和“思蜀”。',
+  },
+  {
+    source: 'sishu_description',
+    target:
+      '出牌阶段开始时，你可以令一名角色于本局游戏中的【乐不思蜀】判定效果反转。',
+  },
+  {
+    source: 'jiang_description',
+    target:
+      '当你使用【决斗】或红色【杀】指定目标后，或成为【决斗】或红色【杀】的目标后，你可以摸一张牌。',
+  },
+  {
+    source: 'hunzi_description',
+    target:
+      '<b>觉醒技</b>，准备阶段开始时，若你的体力值不大于2，你减1点体力上限并获得“英魂”和“英姿”。',
+  },
+  {
+    source: 'zhiba_description',
+    target:
+      '<b>主公技</b>，其他吴势力角色的出牌阶段限一次，其可以与你拼点（若你已发动过“魂姿”，你可以拒绝此拼点），若其没赢，你可以获得双方的拼点牌。',
+  },
+  {
+    source: 'zhijian_description',
+    target:
+      '出牌阶段，你可以将一张装备牌置入一名其他角色的装备区内，然后摸一张牌；当你于出牌阶段使用装备牌时，你可以摸一张牌。',
+  },
+  {
+    source: 'guzheng_description',
+    target:
+      '其他角色的弃牌阶段结束时，你可以将此阶段中该角色因弃置而置入弃牌堆的一张手牌交给其，然后你可以获得其余于此阶段内因弃置而置入弃牌堆的牌。',
+  },
+  {
+    source: 'beige_description',
+    target:
+      '当一名角色受到【杀】造成的伤害后，你可以弃置一张牌，然后令其判定，若结果为：红桃，其回复X点体力（X为伤害值）；方块，其摸三张牌；梅花，伤害来源弃置两张牌；黑桃，伤害来源翻面。',
+  },
+  { source: 'duanchang_description', target: '<b>锁定技</b>，当你死亡时，杀死你的角色失去所有技能。' },
+  {
+    source: 'huashen_description',
+    target:
+      '游戏开始时，你随机将武将牌堆里的三张牌扣置于你的武将牌上，称为“化身”，并亮出其中一张且拥有其上由你选择的一个技能（限定技、觉醒技和主公技除外），然后你的性别及势力视为与此“化身”相同；回合开始或回合结束时，你可以选择一项：1.变更亮出的“化身”；2.移去一至两张未亮出的“化身”并获得等量新“化身”。',
+  },
+  { source: 'xinsheng_description', target: '当你受到1点伤害后，你可以获得一张“化身”。' },
   {
     source: 'gongxin_description',
     target:
