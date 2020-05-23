32cc17241f59c601885f436ea3fbc7880362f98e
donle <donle1988@gmail.com>
2020-05-23 23:11:36 +1000

    remove unused import

--- src/core/skills/characters/standard/xiaoji.ts
+++ src/core/skills/characters/standard/xiaoji.ts
@@ -1,7 +1,7 @@
 import { CardMoveArea, GameEventIdentifiers, ServerEventFinder } from 'core/event/event';
 import { AllStage, CardMoveStage } from 'core/game/stage_processor';
 import { Player } from 'core/player/player';
-import { PlayerCardsArea, PlayerId } from 'core/player/player_props';
+import { PlayerCardsArea } from 'core/player/player_props';
 import { Room } from 'core/room/room';
 import { CommonSkill, TriggerSkill } from 'core/skills/skill';
 
