1422c2d50ff731687253f04cefcb884a835921f0
donle <donle1988@gmail.com>
2020-05-28 20:29:46 +1000

    remove lord tag of yuji

--- src/core/characters/wind/yuji.ts
+++ src/core/characters/wind/yuji.ts
@@ -1,10 +1,9 @@
 import { GameCharacterExtensions } from 'core/game/game_props';
 import { SkillLoader } from 'core/game/package_loader/loader.skills';
-import { Character, CharacterGender, CharacterNationality, Lord } from '../character';
+import { Character, CharacterGender, CharacterNationality } from '../character';
 
 const skillLoaderInstance = SkillLoader.getInstance();
 
-@Lord
 export class YuJi extends Character {
   constructor(id: number) {
     super(id, 'yuji', CharacterGender.Male, CharacterNationality.Qun, 3, 3, GameCharacterExtensions.Wind, [
