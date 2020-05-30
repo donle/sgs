eb8d0c3b7be16dfc1e4c04891eee87111179d021
doublebit <doublebit@qq.com>
2020-05-30 15:28:57 +0800

    tslint

--- src/core/skills/cards/standard/hanbingjian.ts
+++ src/core/skills/cards/standard/hanbingjian.ts
@@ -1,11 +1,5 @@
 import { CardChoosingOptions } from 'core/cards/libs/card_props';
-import {
-  CardMoveReason,
-  ClientEventFinder,
-  EventPacker,
-  GameEventIdentifiers,
-  ServerEventFinder,
-} from 'core/event/event';
+import { CardMoveReason, EventPacker, GameEventIdentifiers, ServerEventFinder } from 'core/event/event';
 import { Sanguosha } from 'core/game/engine';
 import { AllStage, DamageEffectStage } from 'core/game/stage_processor';
 import { Player } from 'core/player/player';
