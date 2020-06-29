4123049dafb88834ff463ee616dba42775ac0042
doublebit <doublebit@qq.com>
2020-06-29 16:43:50 +0800

    add remote images

--- src/ui/platforms/desktop/src/image_loader/dev_image_loader.ts
+++ src/ui/platforms/desktop/src/image_loader/dev_image_loader.ts
@@ -2,53 +2,59 @@ import { PlayerRole } from 'core/player/player_props';
 import { Functional } from 'core/shares/libs/functional';
 import { ImageLoader } from './image_loader';
 
+const remoteRoot: string = 'http://doublebit.gitee.io/pictest/backup_remote';
+
 export class DevImageLoader implements ImageLoader {
   public async getCardImage(name: string) {
     return {
+      src: `${remoteRoot}/images/cards/${name}.webp`,
       alt: name,
     };
   }
 
-  public async getCharacterImage(name: string) {
+  public async getCharacterImage(characterName: string) {
     return {
-      alt: name,
+      src: `${remoteRoot}/images/characters/${characterName}.png`,
+      alt: characterName,
     };
   }
 
   public getCardBack() {
-    return { alt: 'New QSanguosha' };
+    return { src: `${remoteRoot}/images/cards/cardback.webp`, alt: 'New QSanguosha' };
   }
 
-  public async getPlayerRoleCard(role: PlayerRole) {
-    return { alt: Functional.getPlayerRoleRawText(role) };
+  public async getPlayerRoleCard(roleName: PlayerRole) {
+    return { src: `${remoteRoot}/images/system/death/${roleName}.png`, alt: Functional.getPlayerRoleRawText(roleName) };
   }
   public async getOthersEquipCard(cardName: string) {
-    return { alt: cardName };
+    return { src: `${remoteRoot}/images/others_equips/${cardName}.png`, alt: cardName };
   }
 
   public getBackgroundImage() {
     return { alt: '' };
   }
+
   public getEmptySeatImage() {
-    return { alt: '' };
+    return { src: `${remoteRoot}/images/system/empty_seat.png`, alt: '' };
   }
+
   public getUnknownCharacterImage() {
-    return { alt: '' };
+    return { src: `${remoteRoot}/images/system/player_seat.png`, alt: '' };
   }
 
-  async getSlimEquipCard(cardName: string) {
-    return { alt: '' };
+  public async getSlimEquipCard(cardName: string) {
+    return { src: `${remoteRoot}/images/slim_equips/${cardName}.png`, alt: 'Slim Equip Card' };
   }
 
-  async getSlimCard(cardName: string) {
-    return { alt: '' };
+  public async getSlimCard(cardName: string) {
+    return { src: `${remoteRoot}/images/slim_cards/${cardName}.png`, alt: 'Slim Card' };
   }
 
-  getCardNumberBgImage() {
-    return { alt: '' };
+  public getCardNumberBgImage() {
+    return { src: `${remoteRoot}/images/system/cardNumBg.png`, alt: '' };
   }
 
-  getTurnedOverCover() {
-    return { alt: '' };
+  public getTurnedOverCover() {
+    return { src: `${remoteRoot}/images/system/turn_over.png`, alt: '' };
   }
 }
