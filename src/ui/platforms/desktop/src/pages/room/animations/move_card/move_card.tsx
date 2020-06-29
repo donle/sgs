ea6090a8aebb5e82b499a530f095956401629909
donle <donle1988@gmail.com>
2020-06-29 13:19:54 +1000

    lint

--- src/ui/platforms/desktop/src/pages/room/animations/move_card/move_card.tsx
+++ src/ui/platforms/desktop/src/pages/room/animations/move_card/move_card.tsx
@@ -1,6 +1,6 @@
 import { Card } from 'core/cards/card';
 import { CardId } from 'core/cards/libs/card_props';
-import { CardMoveArea, GameEventIdentifiers, ServerEventFinder, CardMoveReason } from 'core/event/event';
+import { CardMoveArea, CardMoveReason, GameEventIdentifiers, ServerEventFinder } from 'core/event/event';
 import { Sanguosha } from 'core/game/engine';
 import { PlayerId } from 'core/player/player_props';
 import { ClientTranslationModule } from 'core/translations/translation_module.client';
