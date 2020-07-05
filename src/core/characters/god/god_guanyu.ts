9b20139e9797c26e218990cbb56bd5a4495a71ea
doublebit <doublebit@qq.com>
2020-07-05 09:52:05 +0800

    add god_guanyu

---
+++ src/core/characters/god/god_guanyu.ts
@@ -0,0 +1,15 @@
+import { GameCharacterExtensions } from 'core/game/game_props';
+import { SkillLoader } from 'core/game/package_loader/loader.skills';
+import { Character, CharacterGender, CharacterNationality, Lord } from '../character';
+
+const skillLoaderInstance = SkillLoader.getInstance();
+
+@Lord
+export class GodGuanYu extends Character {
+  constructor(id: number) {
+    super(id, 'god_guanyu', CharacterGender.Male, CharacterNationality.God, 5, 5, GameCharacterExtensions.God, [
+      ...skillLoaderInstance.getSkillsByName('wushen'),
+      ...skillLoaderInstance.getSkillsByName('wuhun'),
+    ]);
+  }
+}
