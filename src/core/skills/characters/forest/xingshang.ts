610063a5db5c36f7bf8d6c021534862ca4844a08
tassel <p444222@yeah.net>
2020-07-07 08:45:29 +0800

    Caopi Complete (#28)
    * create Forest package and Caopi (include fangzhu)
    
    * add Xingshang & fix any mistake in fangzhu
    
    * add Songwei
    
    * fix any mistake
    
    * fix songwei
    
    * fix drawCards func & fix fangzhu options
    
    * fix songwei skill log mistake
    
    * fix Jiuyuan bug
    
    * create Forest package and Caopi (include fangzhu)
    
    * add Xingshang & fix any mistake in fangzhu
    
    * add Songwei
    
    * fix any mistake
    
    * fix songwei
    
    * fix drawCards func & fix fangzhu options
    
    * fix songwei skill log mistake
    
    * fix Jiuyuan bug
    
    * fix any mistake from Caopi
    
    * fix bugs from Xingshang & Fangzhu & Jiuyuan

---
+++ src/core/skills/characters/forest/xingshang.ts
@@ -0,0 +1,85 @@
+import { Card } from 'core/cards/card';
+import { CardMoveArea, CardMoveReason, EventPacker, GameEventIdentifiers, ServerEventFinder } from 'core/event/event';
+import { AllStage, PlayerDiedStage } from 'core/game/stage_processor';
+import { Player } from 'core/player/player';
+import { Room } from 'core/room/room';
+import { TriggerSkill } from 'core/skills/skill';
+import { CommonSkill } from 'core/skills/skill_wrappers';
+
+@CommonSkill({name: 'xingshang', description: 'xingshang_description'})
+export class Xingshang extends TriggerSkill {
+  isTriggerable(event: ServerEventFinder<GameEventIdentifiers.PlayerDiedEvent>, stage?: AllStage) {
+    return stage === PlayerDiedStage.PlayerDied;
+  }
+
+  canUse(room: Room, owner: Player, content: ServerEventFinder<GameEventIdentifiers.PlayerDiedEvent>) {
+    const to = room.getPlayerById(content.playerId);
+    return owner.Id !== content.playerId && !(owner.Hp >= owner.MaxHp && to.getPlayerCards().length <= 0);
+  }
+
+  async onTrigger() {
+    return true;
+  }
+
+  async onEffect(room: Room, skillUseEvent: ServerEventFinder<GameEventIdentifiers.SkillEffectEvent>) {
+    const { triggeredOnEvent } = skillUseEvent;
+    const { playerId } = triggeredOnEvent as ServerEventFinder<GameEventIdentifiers.PlayerDiedEvent>;
+    const fromId = skillUseEvent.fromId;
+    const dead = room.getPlayerById(playerId);
+    const caopi = room.getPlayerById(fromId);
+
+    if (dead.getPlayerCards().length <= 0) {
+      room.recover({
+        recoveredHp: 1,
+        toId: fromId,
+        recoverBy: fromId,
+      });
+    } else if (caopi.Hp >= caopi.MaxHp) {
+      const heritage = dead.getPlayerCards();
+      await room.moveCards({
+        movingCards: heritage.map(cardId => ({card: cardId, fromArea: dead.cardFrom(cardId)})),
+        fromId: playerId,
+        moveReason: CardMoveReason.ActivePrey,
+        toId: fromId,
+        toArea: CardMoveArea.HandArea,
+        proposer: fromId,
+        movedByReason: this.Name,
+      });
+    } else {
+      const askForOptionsEvent: ServerEventFinder<GameEventIdentifiers.AskForChoosingOptionsEvent> = {
+        options: ['xingshang:recover', 'xingshang:pickup'],
+        conversation: 'please choose',
+        toId: fromId,
+      };
+
+      room.notify(
+        GameEventIdentifiers.AskForChoosingOptionsEvent,
+        EventPacker.createUncancellableEvent<GameEventIdentifiers.AskForChoosingOptionsEvent>(askForOptionsEvent),
+        fromId,
+      );
+
+      const response = await room.onReceivingAsyncReponseFrom(GameEventIdentifiers.AskForChoosingOptionsEvent, fromId);
+      response.selectedOption = response.selectedOption || 'xingshang:pickup';
+      if (response.selectedOption === 'xingshang:recover') {
+        room.recover({
+          recoveredHp: 1,
+          toId: fromId,
+          recoverBy: fromId,
+        });
+      } else {
+        const heritage = dead.getPlayerCards();
+        await room.moveCards({
+          movingCards: heritage.map(cardId => ({card: cardId, fromArea: dead.cardFrom(cardId)})),
+          fromId: playerId,
+          moveReason: CardMoveReason.ActivePrey,
+          toId: fromId,
+          toArea: CardMoveArea.HandArea,
+          proposer: fromId,
+          movedByReason: this.Name,
+        });
+      }
+    }
+
+    return true;
+  }
+}
