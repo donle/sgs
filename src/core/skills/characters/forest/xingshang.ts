108fb650c13b9356770c6040ac2762d050ccbcfa
CompulsorySkill <p444222@yeah.net>
2020-07-07 22:48:27 +0800

    fix warning from Xingshang fix skill Dimeng

--- src/core/skills/characters/forest/xingshang.ts
+++ src/core/skills/characters/forest/xingshang.ts
@@ -1,4 +1,3 @@
-import { Card } from 'core/cards/card';
 import { CardMoveArea, CardMoveReason, EventPacker, GameEventIdentifiers, ServerEventFinder } from 'core/event/event';
 import { AllStage, PlayerDiedStage } from 'core/game/stage_processor';
 import { Player } from 'core/player/player';
