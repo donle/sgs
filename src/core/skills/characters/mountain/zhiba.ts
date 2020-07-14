2686106e7dd0746e669cb4592b01cafc85c1ca41
Ho-spair <linyuy@163.com>
2020-07-14 23:46:32 +0800

    add zhiba

---
+++ src/core/skills/characters/mountain/zhiba.ts
@@ -0,0 +1,155 @@
+import { CardId } from 'core/cards/libs/card_props';
+import { CharacterNationality } from 'core/characters/character';
+import {
+  CardMoveArea,
+  CardMoveReason,
+  EventPacker,
+  GameEventIdentifiers,
+  ServerEventFinder,
+} from 'core/event/event';
+import { AllStage, GameStartStage, PlayerPhase } from 'core/game/stage_processor';
+import { Player } from 'core/player/player';
+import { PlayerCardsArea, PlayerId } from 'core/player/player_props';
+import { Room } from 'core/room/room';
+import { ActiveSkill, CommonSkill, LordSkill, SideEffectSkill, TriggerSkill } from 'core/skills/skill';
+import { TranslationPack } from 'core/translations/translation_json_tool';
+import { HunZi } from './hunzi';
+
+@LordSkill
+@CommonSkill({ name: 'zhiba', description: 'zhiba_description' })
+export class ZhiBa extends TriggerSkill {
+  public isAutoTrigger() {
+    return true;
+  }
+
+  public isTriggerable(event: ServerEventFinder<GameEventIdentifiers.GameStartEvent>, stage?: AllStage): boolean {
+    return stage === GameStartStage.BeforeGameStart;
+  }
+  public canUse(room: Room, owner: Player, content: ServerEventFinder<GameEventIdentifiers>): boolean {
+    return true;
+  }
+  public async onTrigger(room: Room, event: ServerEventFinder<GameEventIdentifiers.SkillUseEvent>): Promise<boolean> {
+    event.translationsMessage = undefined;
+    return true;
+  }
+  public async onEffect(room: Room, event: ServerEventFinder<GameEventIdentifiers.SkillUseEvent>): Promise<boolean> {
+    for (const player of room.getOtherPlayers(event.fromId)) {
+      if (player.Nationality === CharacterNationality.Wu) {
+        room.obtainSkill(player.Id, ZhiBaPindianCard.Name);
+      }
+    }
+
+    return true;
+  }
+}
+
+@SideEffectSkill
+@CommonSkill({ name: ZhiBa.Name, description: ZhiBa.Description })
+export class ZhiBaPindianCard extends ActiveSkill {
+  public canUse(room: Room, owner: Player) {
+    return (
+      owner.hasUsedSkillTimes(this.Name) < room.getAlivePlayersFrom()
+        .filter(player => player.hasSkill(this.GeneralName)).length &&
+      owner.getCardIds(PlayerCardsArea.HandArea).length > 0
+    );
+  }
+
+  public numberOfTargets() {
+    return 1;
+  }
+
+  public cardFilter(room: Room, owner: Player, cards: CardId[]): boolean {
+    return cards.length === 0;
+  }
+
+  public isAvailableCard(owner: PlayerId, room: Room, cardId: CardId): boolean {
+    return false;
+  }
+
+  public isAvailableTarget(owner: PlayerId, room: Room, target: PlayerId): boolean {
+    return (
+      room.getPlayerById(target).hasSkill(this.GeneralName) &&
+      room.Analytics.getRecordEvents<GameEventIdentifiers.SkillEffectEvent>(
+        event =>
+          event.skillName === this.Name &&
+          event.fromId === owner &&
+          event.toIds !== undefined &&
+          event.toIds?.includes(target),
+        owner,
+        true,
+        [PlayerPhase.PlayCardStage],
+      ).length === 0
+    );
+  }
+
+  public async onUse(room: Room, event: ServerEventFinder<GameEventIdentifiers.SkillUseEvent>): Promise<boolean> {
+    return true;
+  }
+
+  public async onEffect(room: Room, event: ServerEventFinder<GameEventIdentifiers.SkillEffectEvent>): Promise<boolean> {
+    const { fromId, toIds } = event;
+    const toId = toIds![0];
+
+    let selectedOption: string = 'yes';
+    if (room.getPlayerById(toId).hasUsedSkill(HunZi.Name)) {
+      const options: string[] = ['yes', 'no'];
+
+      const askForChooseEvent = EventPacker.createUncancellableEvent<GameEventIdentifiers.AskForChoosingOptionsEvent>({
+        options,
+        conversation: TranslationPack.translationJsonPatcher(
+          '{0}: do you agree to pindian with {1}',
+          this.Name,
+          TranslationPack.patchPlayerInTranslation(room.getPlayerById(fromId)),
+        ).extract(),
+        toId,
+      });
+
+      room.notify(GameEventIdentifiers.AskForChoosingOptionsEvent, askForChooseEvent, toId);
+
+      const response = await room.onReceivingAsyncResponseFrom(GameEventIdentifiers.AskForChoosingOptionsEvent, toId);
+      selectedOption = response.selectedOption || 'no';
+    }
+
+    if (selectedOption === 'yes') {
+      const pindianResult = await room.pindian(fromId, toIds!);
+      if (!pindianResult) {
+        return false;
+      }
+
+      if (!pindianResult.winners.includes(fromId)) {
+        const options: string[] = ['confirm', 'cancel'];
+        const pindianCardIds = pindianResult.pindianCards.reduce<CardId[]>((allCards, card) => {
+          return [...allCards, card.cardId];
+        }, []);
+
+        const askForChooseEvent = EventPacker.createUncancellableEvent<GameEventIdentifiers.AskForChoosingOptionsEvent>({
+          options,
+          conversation: TranslationPack.translationJsonPatcher(
+            '{0}: do you want to obtain pindian cards: {1}',
+            this.Name,
+            TranslationPack.patchCardInTranslation(...pindianCardIds),
+          ).extract(),
+          toId,
+        });
+
+        room.notify(GameEventIdentifiers.AskForChoosingOptionsEvent, askForChooseEvent, toId);
+
+        const response = await room.onReceivingAsyncResponseFrom(GameEventIdentifiers.AskForChoosingOptionsEvent, toId);
+        response.selectedOption = response.selectedOption || 'confirm';
+
+        if (response.selectedOption === 'confirm') {
+          await room.moveCards({
+            movingCards: pindianCardIds.map(card => ({ card, fromArea: CardMoveArea.DropStack })),
+            toId,
+            toArea: CardMoveArea.HandArea,
+            moveReason: CardMoveReason.ActivePrey,
+            proposer: toId,
+            movedByReason: this.Name,
+          });
+        }
+      }
+    }
+
+    return true;
+  }
+}
--- src/ui/platforms/desktop/src/languages/zh_CN/translations.ts
+++ src/ui/platforms/desktop/src/languages/zh_CN/translations.ts
@@ -310,6 +310,7 @@ export const characterDictionary: Word[] = [
   { source: 'jiang', target: '激昂' },
   { source: 'hunzi', target: '魂姿' },
   { source: 'zhiba', target: '制霸' },
+  { source: '~zhiba', target: '制霸' },
 
   { source: 'zhangzhaozhanghong', target: '张昭张纮' },
   { source: 'zhijian', target: '直谏' },
@@ -1329,6 +1330,14 @@ export const eventDictionary: Word[] = [
   {
     source: '{0} triggered skill {1}, prevent the damage',
     target: '{0} 触发了 “{1}” 的效果，防止了此伤害',
+  },
+  {
+    source: '{0}: do you agree to pindian with {1}',
+    target: '{0}：你是否同意和 {1} 进行拼点？'
+  },
+  {
+    source: '{0}: do you want to obtain pindian cards: {1}',
+    target: '{0}：你可以获得拼点牌 {1}'
   },
   {
     source: 'please drop a {0} hand card to hit {1} 1 hp of damage type fire',
