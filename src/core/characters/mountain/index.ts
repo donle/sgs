b78502411486df35294eaeed38975801ac1f4133
Ho-spair <linyuy@163.com>
2020-07-15 20:54:07 +0800

    add dengai

--- src/core/characters/mountain/index.ts
+++ src/core/characters/mountain/index.ts
@@ -1,8 +1,11 @@
 import { Character } from '../character';
+import { DengAi } from './dengai';
 import { ErZhang } from './erzhang';
 import { SunCe } from './sunce';
 
 export const MountainCharacterPackage: (index: number) => Character[] = index => [
+  new DengAi(index++),
+
   new SunCe(index++),
   new ErZhang(index++),
 ];
