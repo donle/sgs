c510991cfb5c8aca8267bd4cbf34dfc249c29d5a
CompulsorySkill <p444222@yeah.net>
2020-07-22 11:45:37 +0800

    create Character Jiangwei with Skill Tiaoxin & Zhiji

--- src/core/skills/index.ts
+++ src/core/skills/index.ts
@@ -156,6 +156,8 @@ export { WeiMu } from './characters/forest/weimu';
 export { DuanLiang, DuanLiangShadow } from './characters/forest/duanliang';
 export { JieZi } from './characters/forest/jiezi';
 
+export { TiaoXin } from './characters/mountain/tiaoxin';
+export { ZhiJi } from './characters/mountain/zhiji';
 export { JiAng } from './characters/mountain/jiang';
 export { HunZi } from './characters/mountain/hunzi';
 export { ZhiBa, ZhiBaPindianCard } from './characters/mountain/zhiba';
