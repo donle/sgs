8d334f561a509b5ab69d0b0539c573c4893be6f5
doublebit <fan1053038532@163.com>
2020-08-16 19:28:29 +0800

    Cao zhi (#62)

--- src/core/characters/yijiang2011/index.ts
+++ src/core/characters/yijiang2011/index.ts
@@ -1,8 +1,10 @@
 import { Character } from '../character';
+import { CaoZhi } from './caozhi';
 import { MaSu } from './masu';
 import { XuShu } from './xushu';
 
 export const YiJiang2011Package: (index: number) => Character[] = index => [
   new MaSu(index++),
   new XuShu(index++),
+  new CaoZhi(index++),
 ];
--- src/core/event/event.ts
+++ src/core/event/event.ts
@@ -204,6 +204,10 @@ export const serverResponsiveListenerEvents = [
   GameEventIdentifiers.AskForContinuouslyChoosingCardEvent,
 ];
 
+export const enum CardMovedBySpecifiedReason {
+  JudgeProcess = 'JudgeProcess',
+}
+
 export const enum CardMoveReason {
   CardDraw,
   ActivePrey,
