c96761840e3c7f8f667e25426ee452cb8124fdf2
doublebit <fan1053038532@163.com>
2020-08-17 17:00:18 +0800

    add XuSheng (#64)

--- src/core/characters/yijiang2011/index.ts
+++ src/core/characters/yijiang2011/index.ts
@@ -1,10 +1,12 @@
 import { Character } from '../character';
 import { CaoZhi } from './caozhi';
 import { MaSu } from './masu';
+import { XuSheng } from './xusheng';
 import { XuShu } from './xushu';
 
 export const YiJiang2011Package: (index: number) => Character[] = index => [
   new MaSu(index++),
   new XuShu(index++),
+  new XuSheng(index++),
   new CaoZhi(index++),
 ];
