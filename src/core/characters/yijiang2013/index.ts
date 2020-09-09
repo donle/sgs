d95c3541dbcc9db886e26e189da8287de8259d46
CompulsorySkill <p444222@yeah.net>
2020-09-10 00:55:19 +0800

    create package & add translations

---
+++ src/core/characters/yijiang2013/index.ts
@@ -0,0 +1,15 @@
+import { Character } from '../character';
+
+export const YiJiang2013Package: (index: number) => Character[] = index => [
+  // new CaoChong(index++),
+  // new FuHuangHou(index++),
+  // new GuanPing(index++),
+  // new GuoHuai(index++),
+  // new JianYong(index++),
+  // new LiRu(index++),
+  // new LiuFeng(index++),
+  // new ManChong(index++),
+  // new PanZhangMaZhong(index++),
+  // new YuFan(index++),
+  // new ZhuRan(index++),
+];
