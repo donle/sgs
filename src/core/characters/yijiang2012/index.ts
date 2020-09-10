340c1f0f2f837e45711b6678d18897cf89b0e722
Ho-spair <linyuy@163.com>
2020-09-10 23:12:42 +0800

    add ZhongHui

--- src/core/characters/yijiang2012/index.ts
+++ src/core/characters/yijiang2012/index.ts
@@ -1,4 +1,8 @@
 import { Character } from '../character';
 import { XunYou } from './xunyou';
+import { ZhongHui } from './zhonghui';
 
-export const YiJiang2012Package: (index: number) => Character[] = index => [new XunYou(index++)];
+export const YiJiang2012Package: (index: number) => Character[] = index => [
+  new XunYou(index++),
+  new ZhongHui(index++),
+];
