749cb2bfbab593f8a67dc6b1d0e7b7821020c625
CompulsorySkill <p444222@yeah.net>
2020-09-12 13:16:32 +0800

    add package translations

--- src/ui/platforms/desktop/src/languages/zh_CN/translations/index.ts
+++ src/ui/platforms/desktop/src/languages/zh_CN/translations/index.ts
@@ -8,6 +8,7 @@ import * as mountainCharactersDictionary from './mountain';
 import * as standardCharactersDictionary from './standard';
 import * as windCharactersDictionary from './wind';
 import * as yiJiangCharactersDictionary from './yijiang2011';
+import * as yiJiang3CharactersDictionary from './yijiang2013'
 
 export {
   cardsDictionary,
@@ -20,4 +21,5 @@ export {
   windCharactersDictionary,
   yiJiangCharactersDictionary,
   oneVersusTwoCharactersDictonary,
+  yiJiang3CharactersDictionary,
 };
