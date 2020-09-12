6b3e5c9557afb8ecb49ce021b62d54f8b93f39f3
Ho-spair <62695577+Ho-spair@users.noreply.github.com>
2020-09-12 23:04:57 +0800

    add ZhongHui (#79)

--- src/core/skills/index.ts
+++ src/core/skills/index.ts
@@ -232,6 +232,9 @@ export { XuanFeng, XuanFengMove, XuanFengDrop } from './characters/yijiang2011/x
 
 export { QiCe } from './characters/yijiang2012/qice';
 export { ZhiYu } from './characters/yijiang2012/zhiyu';
+export { QuanJi, QuanJiShadow, QuanJiBuff } from './characters/yijiang2012/quanji';
+export { ZiLi } from './characters/yijiang2012/zili';
+export { PaiYi } from './characters/yijiang2012/paiyi';
 
 export { Cheat } from './system/cheat';
 export { BaHu, BaHuShadow } from './game_mode/1v2/bahu';
