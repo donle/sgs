485e11ec32a134ad4558001b956a98a731a4f89b
kenwosai <1164198126@qq.com>
2020-09-14 20:09:09 +0800

    duodao

--- src/core/skills/characters/yijiang2013/duodao.ts
+++ src/core/skills/characters/yijiang2013/duodao.ts
@@ -51,15 +51,6 @@ export class DuoDao extends TriggerSkill {
       if (weapon === undefined) {
         return true;
       }
-      const damageFrom = room.getPlayerById(fromId);
-      const options: CardChoosingOptions = {
-        [PlayerCardsArea.EquipArea]: damageFrom.getCardIds(PlayerCardsArea.EquipArea),
-      };
-      const chooseCardEvent = {
-        fromId: skillUseEvent.fromId!,
-        toId: fromId,
-        options,
-      };
       await room.moveCards({
         movingCards: [{ card: weapon, fromArea: CardMoveArea.EquipArea }],
         fromId,
