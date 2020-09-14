02dabdb012efab362d3c218fb6e4fe9ca3d9c3f3
kenwosai <1164198126@qq.com>
2020-09-14 09:19:06 +0800

    修改

--- src/core/skills/characters/yijiang2013/duodao.ts
+++ src/core/skills/characters/yijiang2013/duodao.ts
@@ -1,10 +1,10 @@
 import { CardType } from 'core/cards/card';
-import { CardId } from 'core/cards/libs/card_props';
+import { CardChoosingOptions, CardId } from 'core/cards/libs/card_props';
 import { CardMoveArea, CardMoveReason, GameEventIdentifiers, ServerEventFinder } from 'core/event/event';
 import { Sanguosha } from 'core/game/engine';
 import { AimStage, AllStage } from 'core/game/stage_processor';
 import { Player } from 'core/player/player';
-import { PlayerId } from 'core/player/player_props';
+import { PlayerCardsArea, PlayerId } from 'core/player/player_props';
 import { Room } from 'core/room/room';
 import { CommonSkill, TriggerSkill } from 'core/skills/skill';
 @CommonSkill({ name: 'duodao', description: 'duodao_description' })
@@ -51,6 +51,15 @@ export class DuoDao extends TriggerSkill {
       if (weapon === undefined) {
         return true;
       }
+      const damageFrom = room.getPlayerById(fromId);
+      const options: CardChoosingOptions = {
+        [PlayerCardsArea.EquipArea]: damageFrom.getCardIds(PlayerCardsArea.EquipArea),
+      };
+      const chooseCardEvent = {
+        fromId: skillUseEvent.fromId!,
+        toId: fromId,
+        options,
+      };
       await room.moveCards({
         movingCards: [{ card: weapon, fromArea: CardMoveArea.EquipArea }],
         fromId,
