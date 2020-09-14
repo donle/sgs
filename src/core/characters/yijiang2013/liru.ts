82c2f43cf922fa212a5b5b977d57e4870e83533e
kenwosai <1164198126@qq.com>
2020-09-14 23:15:56 +0800

    mieji

--- src/core/characters/yijiang2013/liru.ts
+++ src/core/characters/yijiang2013/liru.ts
@@ -1,6 +1,7 @@
 import { GameCharacterExtensions } from 'core/game/game_props';
 import { SkillLoader } from 'core/game/package_loader/loader.skills';
 import { JueCe } from 'core/skills/characters/yijiang2013/juece';
+import { MieJi } from 'core/skills/characters/yijiang2013/mieji';
 import { Character, CharacterGender, CharacterNationality } from '../character';
 
 const skillLoaderInstance = SkillLoader.getInstance();
@@ -9,6 +10,7 @@ export class LiRu extends Character {
   constructor(id: number) {
     super(id, 'liru', CharacterGender.Male, CharacterNationality.Qun, 3, 3, GameCharacterExtensions.YiJiang2013, [
       skillLoaderInstance.getSkillByName(JueCe.Name),
+      skillLoaderInstance.getSkillByName(MieJi.Name),
     ]);
   }
 }
