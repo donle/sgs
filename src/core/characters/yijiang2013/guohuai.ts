24ceb3e74ae7eff3cdc9bc5b1bc70d79cf83b4ee
Notify-ctrl <1969315774@qq.com>
2020-12-10 13:19:02 +0800

    modified skill 'jingce' and its description

--- src/core/characters/yijiang2013/guohuai.ts
+++ src/core/characters/yijiang2013/guohuai.ts
@@ -1,6 +1,5 @@
 import { GameCharacterExtensions } from 'core/game/game_props';
 import { SkillLoader } from 'core/game/package_loader/loader.skills';
-import { JingCe } from 'core/skills/characters/yijiang2013/jingce';
 import { Character, CharacterGender, CharacterNationality } from '../character';
 
 const skillLoaderInstance = SkillLoader.getInstance();
@@ -8,7 +7,7 @@ const skillLoaderInstance = SkillLoader.getInstance();
 export class GuoHuai extends Character {
   constructor(id: number) {
     super(id, 'guohuai', CharacterGender.Male, CharacterNationality.Wei, 4, 4, GameCharacterExtensions.YiJiang2013, [
-      skillLoaderInstance.getSkillByName(JingCe.Name),
+      ...skillLoaderInstance.getSkillsByName('jingce'),
     ]);
   }
 }
