b2cf4ea4a9dd3d6d0496c09daee1d76ad9463dff
doublebit <doublebit@qq.com>
2021-01-08 14:12:00 +0800

    support for skipping the opening animation

--- src/ui/platforms/desktop/src/pages/openning/openning.tsx
+++ src/ui/platforms/desktop/src/pages/openning/openning.tsx
@@ -5,16 +5,27 @@ import { PagePropsWithConfig } from 'types/page_props';
 import styles from './openning.module.css';
 
 export class OpenningPage extends React.PureComponent<PagePropsWithConfig> {
+  private timeoutId: NodeJS.Timeout;
+
+  private readonly jumpToLobby = () => {
+    this.props.history.push('/lobby');
+  }
 
   componentDidMount() {
-    setTimeout(() => {
-      this.props.history.push('/lobby');
+    document.addEventListener('keydown', this.jumpToLobby);
+    this.timeoutId = setTimeout(() => {
+      this.jumpToLobby();
     }, 6000);
   }
 
+ componentWillUnmount() {
+  document.removeEventListener('keydown', this.jumpToLobby);
+  clearTimeout(this.timeoutId);
+ }
+
   render() {
     return (
-      <div className={styles.scene}>
+      <div className={styles.scene} onClick={this.jumpToLobby}>
         <img className={styles.logo} src={logoImage} alt="logo" />
         <img className={styles.description} src={descriptionImage} alt="description" />
       </div>
