a39bb3261a01c619787494de86a2b2e47bed6a4b
Gaeric <1062217098@qq.com>
2021-01-10 15:25:08 +0800

    add character jushou.

---
+++ src/core/characters/yijiang2014/jushou.ts
@@ -0,0 +1,14 @@
+import { GameCharacterExtensions } from 'core/game/game_props';
+import { SkillLoader } from 'core/game/package_loader/loader.skills';
+import { Character, CharacterGender, CharacterNationality } from '../character';
+
+const skillLoaderInstance = SkillLoader.getInstance();
+
+export class JuShou extends Character {
+  constructor(id: number) {
+    super(id, 'jushou', CharacterGender.Male, CharacterNationality.Qun, 3, 3, GameCharacterExtensions.YiJiang2014, [
+      ...skillLoaderInstance.getSkillsByName('shibei'),
+      skillLoaderInstance.getSkillByName('jianying'),
+    ]);
+  }
+}
