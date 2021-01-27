411e3b500f2a37b2fd33f96a47e29f25abc1745f
donle <donle1988@gmail.com>
2021-01-27 22:44:30 +1100

    re-create yjcm 2013 branch

---
+++ src/core/characters/yijiang2013/index.ts
@@ -0,0 +1,18 @@
+import { Character } from '../character';
+import { CaoChong } from './caochong';
+import { GuoHuai } from './guohuai';
+import { ManChong } from './manchong';
+
+export const YiJiang2013Package: (index: number) => Character[] = index => [
+  new CaoChong(index++),
+  // new FuHuangHou(index++),
+  // new GuanPing(index++),
+  new GuoHuai(index++),
+  // new JianYong(index++),
+  // new LiRu(index++),
+  // new LiuFeng(index++),
+  new ManChong(index++),
+  // new PanZhangMaZhong(index++),
+  // new YuFan(index++),
+  // new ZhuRan(index++),
+];
