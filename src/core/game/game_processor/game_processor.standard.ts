3832f06fc81b330827782f48240406e66444bf9f
donle <donle1988@gmail.com>
2021-01-29 15:13:58 +1100

    add killedByCard in dying event

--- src/core/game/game_processor/game_processor.standard.ts
+++ src/core/game/game_processor/game_processor.standard.ts
@@ -1105,6 +1105,8 @@ export class StandardGameProcessor extends GameProcessor {
         const dyingEvent: ServerEventFinder<GameEventIdentifiers.PlayerDyingEvent> = {
           dying: to.Id,
           killedBy: event.fromId,
+          killedByCard: event.cardIds,
+          triggeredBySkills: event.triggeredBySkills,
         };
 
         if (to.Hp <= 0) {
@@ -1148,7 +1150,7 @@ export class StandardGameProcessor extends GameProcessor {
     event: ServerEventFinder<GameEventIdentifiers.PlayerDyingEvent>,
     onActualExecuted?: (stage: GameEventStage) => Promise<boolean>,
   ) {
-    const { dying, killedBy } = event;
+    const { dying, killedBy, killedByCard } = event;
     const to = this.room.getPlayerById(dying);
     this.room.broadcast(GameEventIdentifiers.PlayerDyingEvent, {
       dying: to.Id,
@@ -1156,6 +1158,7 @@ export class StandardGameProcessor extends GameProcessor {
         '{0} is dying',
         TranslationPack.patchPlayerInTranslation(to),
       ).extract(),
+      killedByCard,
     });
 
     to.Dying = true;
