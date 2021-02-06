3cd32c12ae55d3de5577c179ac143ae22fc60d74
kenwosai <1164198126@qq.com>
2021-02-06 15:06:31 +0800

    panzhangmazhong

--- src/core/characters/yijiang2013/index.ts
+++ src/core/characters/yijiang2013/index.ts
@@ -3,6 +3,7 @@ import { CaoChong } from './caochong';
 import { GuoHuai } from './guohuai';
 import { LiuFeng } from './liufeng';
 import { ManChong } from './manchong';
+import { PanZhangMaZhong } from './panzhangmazhong';
 import { YuFan } from './yufan';
 
 export const YiJiang2013Package: (index: number) => Character[] = index => [
@@ -14,7 +15,7 @@ export const YiJiang2013Package: (index: number) => Character[] = index => [
   // new LiRu(index++),
   new LiuFeng(index++),
   new ManChong(index++),
-  // new PanZhangMaZhong(index++),
   new YuFan(index++),
+  new PanZhangMaZhong(index++),
   // new ZhuRan(index++),
 ];
