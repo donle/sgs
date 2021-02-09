9d1daa7ebb310b10faba38e8031fea6646b45f4f
donle <donle1988@gmail.com>
2021-02-09 12:30:36 +1100

    add customized isRefreshAt

--- src/core/skills/characters/yijiang2013/longyin.ts
+++ src/core/skills/characters/yijiang2013/longyin.ts
@@ -3,7 +3,13 @@ import { CardId } from 'core/cards/libs/card_props';
 import { CardMoveReason, EventPacker, GameEventIdentifiers, ServerEventFinder } from 'core/event/event';
 import { Sanguosha } from 'core/game/engine';
 import { GameCommonRules } from 'core/game/game_rules';
-import { AllStage, CardUseStage, PhaseStageChangeStage, PlayerPhaseStages } from 'core/game/stage_processor';
+import {
+  AllStage,
+  CardUseStage,
+  PhaseStageChangeStage,
+  PlayerPhase,
+  PlayerPhaseStages,
+} from 'core/game/stage_processor';
 import { Player } from 'core/player/player';
 import { PlayerId } from 'core/player/player_props';
 import { Room } from 'core/room/room';
@@ -27,11 +33,23 @@ export class LongYin extends TriggerSkill {
     return identifier === GameEventIdentifiers.PhaseStageChangeEvent;
   }
 
+  public isRefreshAt(room: Room, owner: Player, stage: PlayerPhase) {
+    return stage === PlayerPhase.PlayCardStage;
+  }
+
+  public whenRefresh(room: Room, owner: Player) {
+    this.clearLongYinHistory(room, owner);
+  }
+
   public canUse(
     room: Room,
     owner: Player,
     content: ServerEventFinder<GameEventIdentifiers.CardUseEvent | GameEventIdentifiers.PhaseStageChangeEvent>,
   ) {
+    if (owner.Dead) {
+      return false;
+    }
+
     const identifier = EventPacker.getIdentifier(content);
     if (identifier === GameEventIdentifiers.CardUseEvent) {
       const event = content as ServerEventFinder<GameEventIdentifiers.CardUseEvent>;
@@ -54,6 +72,25 @@ export class LongYin extends TriggerSkill {
     return true;
   }
 
+  private clearLongYinHistory(room: Room, from: Player) {
+    const targetId = from.getFlag<PlayerId>(this.Name);
+    const extraUse = from.getInvisibleMark(this.Name);
+    if (extraUse === 0 || !targetId) {
+      return;
+    }
+
+    room.syncGameCommonRules(targetId, target => {
+      GameCommonRules.addCardUsableTimes(
+        new CardMatcher({ generalName: ['slash'] }),
+        -extraUse,
+        room.getPlayerById(targetId),
+      );
+      from.removeInvisibleMark(this.Name);
+      from.removeFlag(this.Name);
+    });
+    from.removeFlag(this.Name);
+  }
+
   public async onTrigger(room: Room, event: ServerEventFinder<GameEventIdentifiers.SkillUseEvent>) {
     const { triggeredOnEvent } = event;
     const identifier = EventPacker.getIdentifier(triggeredOnEvent!);
@@ -68,18 +105,7 @@ export class LongYin extends TriggerSkill {
     const from = room.getPlayerById(fromId);
     const identifier = EventPacker.getIdentifier(triggeredOnEvent!);
     if (identifier === GameEventIdentifiers.PhaseStageChangeEvent) {
-      const targetId = from.getFlag<PlayerId>(this.Name);
-      const extraUse = from.getInvisibleMark(this.Name);
-      room.syncGameCommonRules(targetId, target => {
-        GameCommonRules.addCardUsableTimes(
-          new CardMatcher({ generalName: ['slash'] }),
-          -extraUse,
-          room.getPlayerById(targetId),
-        );
-        from.removeInvisibleMark(this.Name);
-        from.removeFlag(this.Name);
-      });
-      from.removeFlag(this.Name);
+      this.clearLongYinHistory(room, from);
     } else {
       const event = triggeredOnEvent as ServerEventFinder<GameEventIdentifiers.CardUseEvent>;
       await room.dropCards(CardMoveReason.SelfDrop, cardIds!, fromId, fromId, this.Name);
