708c8fff54e22c2c8522f18e63437fa8ecce3336
Gaeric <1062217098@qq.com>
2021-05-19 00:09:08 +0800

    reduce the maxhp of pve_boss

--- src/core/characters/pve/pve_chaofeng.ts
+++ src/core/characters/pve/pve_chaofeng.ts
@@ -2,15 +2,13 @@ import { SkillLoader } from 'core/game/package_loader/loader.skills';
 import { Character, CharacterGender, CharacterNationality } from '../character';
 import { GameCharacterExtensions } from 'core/game/game_props';
 import { PveLongLin } from 'core/skills/game_mode/pve/pve_longlin';
-import { PveHuaShen } from 'core/skills';
 
 const skillLoaderInstance = SkillLoader.getInstance();
 
 export class PveChaoFeng extends Character {
   constructor(id: number) {
     super(id, 'pve_chaofeng', CharacterGender.Female, CharacterNationality.God, 4, 4, GameCharacterExtensions.Pve, [
-      skillLoaderInstance.getSkillByName(PveLongLin.Name),
-      skillLoaderInstance.getSkillByName(PveHuaShen.Name),
+      ...skillLoaderInstance.getSkillsByName(PveLongLin.Name),
     ]);
   }
 }
