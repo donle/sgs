790291473c8b3dbe477ca7cea4201a9cebc23f25
Gaeric <1062217098@qq.com>
2021-06-06 01:01:26 +0800

    fixup yuce recover bug

--- src/core/skills/characters/yijiang2013/yuce.ts
+++ src/core/skills/characters/yijiang2013/yuce.ts
@@ -83,7 +83,7 @@ export class YuCe extends TriggerSkill {
           damageFromId,
         );
 
-        if (selectedCards !== undefined && !selectedCards) {
+        if (selectedCards !== undefined && selectedCards.length > 0) {
           await room.dropCards(CardMoveReason.SelfDrop, selectedCards, damageFromId, damageFromId, this.Name);
         } else {
           await room.recover({
