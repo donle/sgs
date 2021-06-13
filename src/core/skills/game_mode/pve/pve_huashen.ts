ac4e504c6ab2cb441487282158f0266ad32f15df
Gaeric <1062217098@qq.com>
2021-06-14 00:10:53 +0800

    optimize pve_huashen

--- src/core/skills/game_mode/pve/pve_huashen.ts
+++ src/core/skills/game_mode/pve/pve_huashen.ts
@@ -39,7 +39,10 @@ export class PveHuaShen extends TriggerSkill {
     }
 
     if (identifier === GameEventIdentifiers.PlayerDyingEvent) {
-      return (event as ServerEventFinder<GameEventIdentifiers.PlayerDyingEvent>).dying === owner.Id;
+      return (
+        (event as ServerEventFinder<GameEventIdentifiers.PlayerDyingEvent>).dying === owner.Id &&
+        room.getMark(owner.Id, MarkEnum.PveHuaShen) > 0
+      );
     }
 
     return false;
@@ -49,9 +52,7 @@ export class PveHuaShen extends TriggerSkill {
     const NewMaxHp = room.getPlayerById(ownerId).MaxHp + 1;
     const chara = PveHuaShen.CHARACTERS[PveHuaShen.CHARACTERS.length - room.getMark(ownerId, MarkEnum.PveHuaShen)];
     room.addMark(ownerId, MarkEnum.PveHuaShen, -1);
-    if (chara === undefined) {
-      return false;
-    }
+
     const nextCharacter = Sanguosha.getCharacterByCharaterName(chara);
 
     const playerPropertiesChangeEvent: ServerEventFinder<GameEventIdentifiers.PlayerPropertiesChangeEvent> = {
@@ -82,8 +83,6 @@ export class PveHuaShen extends TriggerSkill {
     await room.drawCards(room.getMark(ownerId, MarkEnum.PveHuaShen) === 5 ? 0 : 5, ownerId, 'top', ownerId, this.Name);
     player.setHuaShenInfo({ skillName: nextCharacter.Skills[0].GeneralName, characterId: nextCharacter.Id });
     await room.obtainSkill(ownerId, nextCharacter.Skills[0].GeneralName, true);
-
-    return true;
   }
 
   public async onTrigger(): Promise<boolean> {
@@ -101,9 +100,8 @@ export class PveHuaShen extends TriggerSkill {
       room.addMark(event.fromId, MarkEnum.PveHuaShen, PveHuaShen.CHARACTERS.length);
       await this.nextEntity(room, event.fromId);
 
-      const otherPlayers = room.AlivePlayers.filter(player => player.Id !== event.fromId);
-      for (const player of otherPlayers) {
-        const playerPropertiesChangeEvent: ServerEventFinder<GameEventIdentifiers.PlayerPropertiesChangeEvent> = {
+      room.AlivePlayers.filter(player => player.Id !== event.fromId).forEach(player =>
+        room.changePlayerProperties({
           changedProperties: [
             {
               toId: player.Id,
@@ -111,69 +109,57 @@ export class PveHuaShen extends TriggerSkill {
               hp: player.Character.Hp + 1,
             },
           ],
+        }),
+      );
+    } else {
+      await this.nextEntity(room, event.fromId);
+      const otherPlayers = room.AlivePlayers.filter(player => player.Id !== event.fromId);
+
+      for (const player of otherPlayers) {
+        await room.recover({ recoverBy: event.fromId, recoveredHp: 1, toId: player.Id });
+        await room.drawCards(3, player.Id, 'top');
+
+        const pveHuashenCharacters = room.getRandomCharactersFromLoadedPackage(3);
+        const askForChoosingCharacterEvent: ServerEventFinder<GameEventIdentifiers.AskForChoosingCharacterEvent> = {
+          amount: 1,
+          characterIds: pveHuashenCharacters,
+          toId: player.Id,
+          byHuaShen: true,
         };
 
-        room.changePlayerProperties(playerPropertiesChangeEvent);
+        room.notify(GameEventIdentifiers.AskForChoosingCharacterEvent, askForChoosingCharacterEvent, player.Id);
+
+        const { chosenCharacterIds } = await room.onReceivingAsyncResponseFrom(
+          GameEventIdentifiers.AskForChoosingCharacterEvent,
+          player.Id,
+        );
+
+        const options = Sanguosha.getCharacterById(chosenCharacterIds[0])
+          .Skills.filter(skill => !(skill.isShadowSkill() || skill.isLordSkill()))
+          .map(skill => skill.GeneralName);
+
+        const askForChoosingOptionsEvent: ServerEventFinder<GameEventIdentifiers.AskForChoosingOptionsEvent> = {
+          options,
+          toId: player.Id,
+          conversation: 'pve_huashen: please announce a skill to obtain',
+        };
+
+        room.notify(
+          GameEventIdentifiers.AskForChoosingOptionsEvent,
+          EventPacker.createUncancellableEvent<GameEventIdentifiers.AskForChoosingOptionsEvent>(
+            askForChoosingOptionsEvent,
+          ),
+          player.Id,
+        );
+
+        const { selectedOption } = await room.onReceivingAsyncResponseFrom(
+          GameEventIdentifiers.AskForChoosingOptionsEvent,
+          player.Id,
+        );
+
+        await room.obtainSkill(player.Id, selectedOption!, true);
       }
-      return true;
     }
-
-    if (room.getMark(event.fromId, MarkEnum.PveHuaShen) === 0) {
-      return true;
-    }
-
-    const nextCharacter = await this.nextEntity(room, event.fromId);
-    if (!nextCharacter) {
-      return true;
-    }
-
-    const otherPlayers = room.AlivePlayers.filter(player => player.Id !== event.fromId);
-
-    for (const player of otherPlayers) {
-      await room.recover({ recoverBy: event.fromId, recoveredHp: 1, toId: player.Id });
-      await room.drawCards(3, player.Id, 'top');
-
-      const pveHuashenCharacters = room.getRandomCharactersFromLoadedPackage(3);
-      const askForChoosingCharacterEvent: ServerEventFinder<GameEventIdentifiers.AskForChoosingCharacterEvent> = {
-        amount: 1,
-        characterIds: pveHuashenCharacters,
-        toId: player.Id,
-        byHuaShen: true,
-      };
-
-      room.notify(GameEventIdentifiers.AskForChoosingCharacterEvent, askForChoosingCharacterEvent, player.Id);
-
-      const { chosenCharacterIds } = await room.onReceivingAsyncResponseFrom(
-        GameEventIdentifiers.AskForChoosingCharacterEvent,
-        player.Id,
-      );
-
-      const options = Sanguosha.getCharacterById(chosenCharacterIds[0])
-        .Skills.filter(skill => !(skill.isShadowSkill() || skill.isLordSkill()))
-        .map(skill => skill.GeneralName);
-
-      const askForChoosingOptionsEvent: ServerEventFinder<GameEventIdentifiers.AskForChoosingOptionsEvent> = {
-        options,
-        toId: player.Id,
-        conversation: 'pve_huashen: please announce a skill to obtain',
-      };
-
-      room.notify(
-        GameEventIdentifiers.AskForChoosingOptionsEvent,
-        EventPacker.createUncancellableEvent<GameEventIdentifiers.AskForChoosingOptionsEvent>(
-          askForChoosingOptionsEvent,
-        ),
-        player.Id,
-      );
-
-      const { selectedOption } = await room.onReceivingAsyncResponseFrom(
-        GameEventIdentifiers.AskForChoosingOptionsEvent,
-        player.Id,
-      );
-
-      await room.obtainSkill(player.Id, selectedOption!, true);
-    }
-
     return true;
   }
 }
