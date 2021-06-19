f0e9dfe64ec0fca5422bf5ea351e35fbc57b7112
Muiet <867416257@qq.com>
2021-06-19 23:15:41 +0800

    simplify switch_skin.tsx

--- src/ui/platforms/desktop/src/pages/room/ui/switch_avatar/switch_skin.tsx
+++ src/ui/platforms/desktop/src/pages/room/ui/switch_avatar/switch_skin.tsx
@@ -3,56 +3,43 @@ import { PlayerId } from 'core/player/player_props';
 
 export type HistoryCharacterSkin = {
   playerId: PlayerId;
-  historyCharacterName: string;
+  characterName: string;
   skinNameList: string[];
+  skinName: string;
   nextTime: number;
 };
 
 let historyCharacterSkinInfo: HistoryCharacterSkin[] = [];
-export function getSkinName(characterName: string, playerId?: PlayerId) {
-  let newskinNameList: string[] = [];
-  let skinName: string = '';
-  if (playerId === undefined) {
-    return characterName;
-  }
+export function getSkinName(characterName: string, playerId: PlayerId) {
   const currentTime = new Date().getTime();
-  for (const history of historyCharacterSkinInfo) {
-    if (history.historyCharacterName === characterName && history.playerId === playerId) {
-      if (currentTime >= history.nextTime && history.skinNameList.length > 1) {
-        let oldName = history.skinNameList[Math.floor(Math.random() * history.skinNameList.length)];
-        if (oldName) {
-          history.skinNameList.splice(history.skinNameList.indexOf(oldName), 1);
-          history.skinNameList.unshift(oldName);
-        }
-        skinName = history.skinNameList[0];
-        history.nextTime = currentTime + Math.floor(Math.random() * 5 + 5) * 1000;
-        break;
-      } else {
-        skinName = history.skinNameList[0] ? history.skinNameList[0] : characterName;
-      }
-    }
-  }
-  if (skinName === '' && characterName !== undefined && playerId !== undefined) {
-    if (!newskinNameList.includes(characterName)) {
-      newskinNameList.push(characterName);
-    }
-    gameSkinInfo
-      .find(skinInfo => skinInfo.characterName === characterName)
-      ?.skinInfo.filter(skinName => skinName !== undefined)
-      .forEach(skinInfo => {
-        if (!newskinNameList.includes(skinInfo.skinName)) {
-          newskinNameList.push(skinInfo.skinName);
-        }
-      });
+  const history = historyCharacterSkinInfo.find(
+    history => history.characterName === characterName && history.playerId === playerId,
+  );
 
-    const historyCharacterSkin: HistoryCharacterSkin = {
-      playerId: playerId,
-      historyCharacterName: characterName,
-      skinNameList: newskinNameList,
-      nextTime: currentTime + Math.floor(Math.random() * 5 + 5) * 1000,
-    };
-    historyCharacterSkinInfo.push(historyCharacterSkin);
-    skinName = characterName;
+  if (history) {
+    if (currentTime >= history.nextTime) {
+      history.skinName = history.skinNameList[Math.floor(Math.random() * history.skinNameList.length)];
+      history.nextTime = currentTime + Math.floor(Math.random() * 5 + 30) * 1000;
+    }
+    return history.skinName;
   }
-  return skinName;
+
+  const historyCharacterSkin: HistoryCharacterSkin = {
+    playerId: playerId,
+    characterName: characterName,
+    skinNameList: [characterName],
+    skinName: characterName,
+    nextTime: currentTime + Math.floor(Math.random() * 5 + 30) * 1000,
+  };
+
+  gameSkinInfo
+    .find(gameSkinInfo => gameSkinInfo.characterName === characterName)
+    ?.skinInfo.filter(skinName => skinName !== undefined)
+    .forEach(skinInfo => {
+      historyCharacterSkin.skinNameList.push(skinInfo.skinName);
+    });
+
+  historyCharacterSkinInfo.push(historyCharacterSkin);
+
+  return historyCharacterSkin.skinName;
 }
