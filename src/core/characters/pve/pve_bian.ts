42ef5c8f4c48d5618eff1d17a23878a9940320c0
Gaeric <1062217098@qq.com>
2021-06-27 23:20:14 +0800

    change pve_qinlv to stage 2

--- src/core/characters/pve/pve_bian.ts
+++ src/core/characters/pve/pve_bian.ts
@@ -2,6 +2,7 @@ import { SkillLoader } from 'core/game/package_loader/loader.skills';
 import { Character, CharacterGender, CharacterNationality } from '../character';
 import { GameCharacterExtensions } from 'core/game/game_props';
 import { PveSuWei } from 'core/skills/game_mode/pve/pve_suwei';
+import { PveQinLv } from 'core/skills';
 
 const skillLoaderInstance = SkillLoader.getInstance();
 
@@ -9,6 +10,7 @@ export class PveBiAn extends Character {
   constructor(id: number) {
     super(id, 'pve_bian', CharacterGender.Female, CharacterNationality.God, 4, 4, GameCharacterExtensions.Pve, [
       skillLoaderInstance.getSkillByName(PveSuWei.Name),
+      skillLoaderInstance.getSkillByName(PveQinLv.Name),
     ]);
   }
 }
