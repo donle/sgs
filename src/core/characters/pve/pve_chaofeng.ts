5f8e7789e0e0b978583f4b5320c24326cd27ae8f
donle <donle1988@gmail.com>
2021-09-05 00:14:26 +0800

    roll back long shen settings

--- src/core/characters/pve/pve_chaofeng.ts
+++ src/core/characters/pve/pve_chaofeng.ts
@@ -1,6 +1,6 @@
 import { GameCharacterExtensions } from 'core/game/game_props';
 import { SkillLoader } from 'core/game/package_loader/loader.skills';
-import { PveHuaShen, PveJieNu, PveTanSuo } from 'core/skills';
+import { PveJieNu } from 'core/skills';
 import { PveLongLin } from 'core/skills/game_mode/pve/pve_longlin';
 import { Character, CharacterGender, CharacterNationality } from '../character';
 
@@ -11,8 +11,6 @@ export class PveChaoFeng extends Character {
     super(id, 'pve_chaofeng', CharacterGender.Female, CharacterNationality.God, 4, 4, GameCharacterExtensions.Pve, [
       ...skillLoaderInstance.getSkillsByName(PveLongLin.Name),
       skillLoaderInstance.getSkillByName(PveJieNu.Name),
-      skillLoaderInstance.getSkillByName(PveHuaShen.Name),
-      skillLoaderInstance.getSkillByName(PveTanSuo.Name),
     ]);
   }
 }
