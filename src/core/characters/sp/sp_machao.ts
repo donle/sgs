d3a7d5b15edbc84845812eb5c477d5a029080fd9
Gaeric <1062217098@qq.com>
2022-03-24 19:17:20 +0800

    change the gender of sp_machao

--- src/core/characters/sp/sp_machao.ts
+++ src/core/characters/sp/sp_machao.ts
@@ -6,7 +6,7 @@ const skillLorderInstance = SkillLoader.getInstance();
 
 export class SPMaChao extends Character {
   constructor(id: number) {
-    super(id, 'sp_machao', CharacterGender.Female, CharacterNationality.Qun, 4, 4, GameCharacterExtensions.SP, [
+    super(id, 'sp_machao', CharacterGender.Male, CharacterNationality.Qun, 4, 4, GameCharacterExtensions.SP, [
       skillLorderInstance.getSkillByName('zhuiji'),
       skillLorderInstance.getSkillByName('shichou'),
     ]);
