67a6bb9721c13312697ebc5e9862ec09faf047ab
Gaeric <1062217098@qq.com>
2022-04-04 21:32:45 +0800

    improve the skill pve_classic_human

--- src/core/skills/game_mode/pve/pve_classic_human.ts
+++ src/core/skills/game_mode/pve/pve_classic_human.ts
@@ -1,4 +1,4 @@
-import { CardDrawReason, EventPacker, GameEventIdentifiers, ServerEventFinder } from 'core/event/event';
+import { EventPacker, GameEventIdentifiers, ServerEventFinder } from 'core/event/event';
 import { CardSuit } from 'core/cards/libs/card_props';
 import {
   AllStage,
@@ -7,8 +7,8 @@ import {
   DamageEffectStage,
   DrawCardStage,
   PhaseStageChangeStage,
-  PlayerPhase,
   PlayerPhaseStages,
+  StagePriority,
 } from 'core/game/stage_processor';
 import { Player } from 'core/player/player';
 import { Algorithm } from 'core/shares/libs/algorithm';
@@ -17,9 +17,17 @@ import { MarkEnum } from 'core/shares/types/mark_list';
 import { TriggerSkill } from 'core/skills/skill';
 import { CompulsorySkill, ShadowSkill } from 'core/skills/skill_wrappers';
 import { Sanguosha } from 'core/game/engine';
+import { CardMatcher } from 'core/cards/libs/card_matcher';
+import { VirtualCard } from 'core/cards/card';
+import { Slash } from 'core/cards/standard/slash';
 
-// 孤：你每打出或使用一张花色的牌，若没有对应的标记，获得一个标记；回合开始时，消耗四枚标记，增加一点体力上限并回复一点体力；受到伤害时，消耗三枚标记并减少一定伤害，造成伤害时，消耗两枚标记并使伤害值加一；摸牌阶段开始时，消耗一枚标记并多摸一张牌。
-@ShadowSkill
+// 孤勇：
+// 你每打出或使用一张花色的牌，若没有对应的标记，获得一个标记；
+// 当你摸牌时，消耗一枚标记，额外摸一张牌
+// 当你造成或受到伤害时，消耗两枚标记，伤害值+1/-1
+// 准备阶段开始时，消耗三枚标记，摸一张牌并视为使用一张杀
+// 结束阶段结束时，消耗四枚标记，增加一点体力上限并回复一点体力
+
 @CompulsorySkill({ name: 'pve_classic_gu', description: 'pve_classic_gu_desc' })
 export class PveClassicGu extends TriggerSkill {
   private getMarkNumber(owner: Player) {
@@ -38,9 +46,17 @@ export class PveClassicGu extends TriggerSkill {
     ownerMarks.slice(0, num).map(mark => room.addMark(owner.Id, mark, -1));
   }
 
+  public isAutoTrigger(): boolean {
+    return true;
+  }
+
+  public getPriority() {
+    return StagePriority.High;
+  }
+
   public isTriggerable(
     event: ServerEventFinder<
-      GameEventIdentifiers.PhaseChangeEvent | GameEventIdentifiers.DamageEvent | GameEventIdentifiers.DrawCardEvent
+      GameEventIdentifiers.PhaseStageChangeEvent | GameEventIdentifiers.DamageEvent | GameEventIdentifiers.DrawCardEvent
     >,
     stage?: AllStage,
   ) {
@@ -65,21 +81,16 @@ export class PveClassicGu extends TriggerSkill {
       case GameEventIdentifiers.PhaseStageChangeEvent:
         const phaseStageChangeEvent = content as ServerEventFinder<GameEventIdentifiers.PhaseStageChangeEvent>;
         return (
-          markNumber > 3 &&
-          phaseStageChangeEvent.toStage === PlayerPhaseStages.PrepareStageStart &&
-          phaseStageChangeEvent.playerId === owner.Id
+          phaseStageChangeEvent.playerId === owner.Id &&
+          ((markNumber > 3 && phaseStageChangeEvent.toStage === PlayerPhaseStages.PhaseFinishEnd) ||
+            (markNumber > 2 && phaseStageChangeEvent.toStage === PlayerPhaseStages.PrepareStageStart))
         );
       case GameEventIdentifiers.DrawCardEvent:
         const drawCardEvent = content as ServerEventFinder<GameEventIdentifiers.DrawCardEvent>;
-        return (
-          markNumber > 0 &&
-          room.CurrentPlayerPhase === PlayerPhase.DrawCardStage &&
-          drawCardEvent.bySpecialReason === CardDrawReason.GameStage &&
-          drawCardEvent.fromId === owner.Id
-        );
+        return markNumber > 0 && drawCardEvent.fromId === owner.Id;
       case GameEventIdentifiers.DamageEvent:
         const damageEvent = content as ServerEventFinder<GameEventIdentifiers.DamageEvent>;
-        return (damageEvent.toId === owner.Id && markNumber > 2) || (damageEvent.fromId === owner.Id && markNumber > 1);
+        return markNumber > 1 && (damageEvent.toId === owner.Id || damageEvent.fromId === owner.Id);
       default:
         return false;
     }
@@ -98,22 +109,73 @@ export class PveClassicGu extends TriggerSkill {
     const identifier = EventPacker.getIdentifier(event.triggeredOnEvent);
     switch (identifier) {
       case GameEventIdentifiers.PhaseStageChangeEvent:
-        console.log(`add MaxHp`);
-        this.useMark(room, owner, 4);
-        await room.changeMaxHp(event.fromId, 1);
-        await room.recover({ toId: event.fromId, recoveredHp: 1, recoverBy: event.fromId });
+        const phaseStageChangeEvent =
+          event.triggeredOnEvent as ServerEventFinder<GameEventIdentifiers.PhaseStageChangeEvent>;
+        if (this.getMarkNumber(owner) > 3 && phaseStageChangeEvent.toStage === PlayerPhaseStages.PhaseFinishEnd) {
+          console.log(`add MaxHp`);
+          this.useMark(room, owner, 4);
+          await room.changeMaxHp(event.fromId, 1);
+          await room.recover({ toId: event.fromId, recoveredHp: 1, recoverBy: event.fromId });
+        } else if (
+          this.getMarkNumber(owner) > 2 &&
+          phaseStageChangeEvent.toStage === PlayerPhaseStages.PrepareStageStart
+        ) {
+          this.useMark(room, owner, 3);
+          room.drawCards(1, owner.Id, 'top');
+
+          const targets = room
+            .getOtherPlayers(owner.Id)
+            .filter(player =>
+              room.getPlayerById(owner.Id).canUseCardTo(room, new CardMatcher({ generalName: ['slash'] }), player.Id),
+            )
+            .map(player => player.Id);
+
+          if (targets.length > 0) {
+            const resp = await room.doAskForCommonly<GameEventIdentifiers.AskForChoosingPlayerEvent>(
+              GameEventIdentifiers.AskForChoosingPlayerEvent,
+              {
+                players: targets,
+                toId: owner.Id,
+                requiredAmount: 1,
+                conversation: 'pve_classic_guyong: do you want to use a slash?',
+                triggeredBySkills: [this.Name],
+              },
+              owner.Id,
+            );
+
+            if (resp.selectedPlayers && resp.selectedPlayers.length > 0) {
+              await room.useCard({
+                fromId: owner.Id,
+                targetGroup: [resp.selectedPlayers],
+                cardId: VirtualCard.create<Slash>({ cardName: 'slash', bySkill: this.Name }).Id,
+                extraUse: true,
+                triggeredBySkills: [this.Name],
+              });
+            }
+          }
+        }
         return true;
+
       case GameEventIdentifiers.DrawCardEvent:
         const drawCardEvent = event.triggeredOnEvent as ServerEventFinder<GameEventIdentifiers.DrawCardEvent>;
+        if (this.getMarkNumber(owner) === 0) {
+          return false;
+        }
+
         drawCardEvent.drawAmount += 1;
         this.useMark(room, owner, 1);
         return true;
+
       case GameEventIdentifiers.DamageEvent:
         const damageEvent = event.triggeredOnEvent as ServerEventFinder<GameEventIdentifiers.DamageEvent>;
-        if (damageEvent.toId === owner.Id && this.getMarkNumber(owner) > 2) {
-          this.useMark(room, owner, 3);
+        if (this.getMarkNumber(owner) < 2) {
+          return false;
+        }
+
+        if (damageEvent.toId === owner.Id) {
+          this.useMark(room, owner, 2);
           damageEvent.damage--;
-        } else if (damageEvent.fromId === owner.Id && this.getMarkNumber(owner) > 1) {
+        } else if (damageEvent.fromId === owner.Id) {
           this.useMark(room, owner, 2);
           damageEvent.damage++;
         }
@@ -129,6 +191,10 @@ export class PveClassicGu extends TriggerSkill {
 export class PveClassicGuShadow extends TriggerSkill {
   public isAutoTrigger(): boolean {
     return true;
+  }
+
+  public getPriority() {
+    return StagePriority.High;
   }
 
   public isTriggerable(
