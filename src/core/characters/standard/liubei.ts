2c9be93bf73027f162ec6fe6202a7cf961fb6814
Gaeric <1062217098@qq.com>
2022-04-17 23:02:45 +0800

    revert liubei's skill

--- src/core/characters/standard/liubei.ts
+++ src/core/characters/standard/liubei.ts
@@ -10,7 +10,6 @@ export class LiuBei extends Character {
     super(id, 'liubei', CharacterGender.Male, CharacterNationality.Shu, 4, 4, GameCharacterExtensions.Standard, [
       ...skillLoaderInstance.getSkillsByName('rende'),
       ...skillLoaderInstance.getSkillsByName('jijiang'),
-      ...skillLoaderInstance.getSkillsByName('pve_classic_lianzhen'),
     ]);
   }
 }
