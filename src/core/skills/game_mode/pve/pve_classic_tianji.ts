b5ef658661e1281ad1c0ae5ddb9dbe9d0e478e5c
Gaeric <1062217098@qq.com>
2022-04-18 23:21:42 +0800

    more convenient tianji

--- src/core/skills/game_mode/pve/pve_classic_tianji.ts
+++ src/core/skills/game_mode/pve/pve_classic_tianji.ts
@@ -1,9 +1,10 @@
+import { CardId } from 'core/cards/libs/card_props';
 import { CardMoveReason, GameEventIdentifiers, ServerEventFinder } from 'core/event/event';
 import { EventPacker } from 'core/event/event_packer';
 import { DamageType } from 'core/game/game_props';
 import { AllStage, PhaseStageChangeStage, PlayerPhaseStages } from 'core/game/stage_processor';
 import { Player } from 'core/player/player';
-import { PlayerCardsArea } from 'core/player/player_props';
+import { PlayerId } from 'core/player/player_props';
 import { Room } from 'core/room/room';
 import { TriggerSkill } from 'core/skills/skill';
 import { CommonSkill } from 'core/skills/skill_wrappers';
@@ -32,32 +33,31 @@ export class PveClassicTianJi extends TriggerSkill {
     );
   }
 
+  getSkillLog() {
+    return TranslationPack.translationJsonPatcher(
+      'please discard a card to deal 1 thunder damage to current player?',
+    ).extract();
+  }
+
+  cardFilter(room: Room, owner: Player, cards: CardId[]) {
+    return cards.length === 1;
+  }
+
+  isAvailableCard(owner: PlayerId, room: Room, cardId: CardId) {
+    return room.canDropCard(owner, cardId);
+  }
+
   public async onTrigger() {
     return true;
   }
 
-  public async onEffect(room: Room, skillUseEvent: ServerEventFinder<GameEventIdentifiers.SkillEffectEvent>) {
-    const { fromId } = skillUseEvent;
-    const current = room.CurrentPhasePlayer;
-
-    const resp = await room.askForCardDrop(
-      fromId!,
-      1,
-      [PlayerCardsArea.HandArea, PlayerCardsArea.EquipArea],
-      true,
-      undefined,
-      this.Name,
-      TranslationPack.translationJsonPatcher(
-        'please discard a card to deal 1 thunder damage to {0} ?',
-        TranslationPack.patchPlayerInTranslation(room.getPlayerById(current.Id)),
-      ).extract(),
-    );
-
-    if (resp.droppedCards.length > 0) {
-      await room.dropCards(CardMoveReason.SelfDrop, resp.droppedCards, fromId);
+  public async onEffect(room: Room, event: ServerEventFinder<GameEventIdentifiers.SkillEffectEvent>) {
+    if (event.cardIds !== undefined && event.cardIds.length === 1) {
+      await room.dropCards(CardMoveReason.SelfDrop, event.cardIds, event.fromId);
+      const current = room.CurrentPlayer;
       if (!current.Dead) {
         await room.damage({
-          fromId,
+          fromId: event.fromId,
           toId: current.Id,
           damage: 1,
           damageType: DamageType.Thunder,
