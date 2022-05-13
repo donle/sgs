86637c62aa307873a89b761aeb755416d98bcd4d
Gaeric <1062217098@qq.com>
2022-05-13 23:55:44 +0800

    fixup xiansi, #30 could close with this.
    we don't need to trigger beforeUse on client or web

--- src/core/skills/characters/yijiang2013/xiansi.ts
+++ src/core/skills/characters/yijiang2013/xiansi.ts
@@ -1,17 +1,25 @@
-import { VirtualCard } from 'core/cards/card';
+import { Card, VirtualCard } from 'core/cards/card';
 import { CardMatcher } from 'core/cards/libs/card_matcher';
 import { CardChoosingOptions, CardId, CardSuit } from 'core/cards/libs/card_props';
 import { CardMoveArea, CardMoveReason, GameEventIdentifiers, ServerEventFinder } from 'core/event/event';
 import { EventPacker } from 'core/event/event_packer';
 import { Sanguosha } from 'core/game/engine';
-import { AllStage, GameStartStage, PhaseStageChangeStage, PlayerPhaseStages } from 'core/game/stage_processor';
+import {
+  AllStage,
+  CardResponseStage,
+  CardUseStage,
+  GameStartStage,
+  PhaseStageChangeStage,
+  PlayerPhaseStages,
+} from 'core/game/stage_processor';
 import { Player } from 'core/player/player';
 import { PlayerCardsArea, PlayerId } from 'core/player/player_props';
 import { Room } from 'core/room/room';
 import { System } from 'core/shares/libs/system';
-import { GlobalFilterSkill, TriggerSkill, ViewAsSkill } from 'core/skills/skill';
+import { TargetGroupUtil } from 'core/shares/libs/utils/target_group';
+import { TriggerSkill, ViewAsSkill } from 'core/skills/skill';
 import { OnDefineReleaseTiming } from 'core/skills/skill_hooks';
-import { CommonSkill, ShadowSkill, SideEffectSkill } from 'core/skills/skill_wrappers';
+import { CommonSkill, PersistentSkill, ShadowSkill, SideEffectSkill } from 'core/skills/skill_wrappers';
 
 @CommonSkill({ name: 'xiansi', description: 'xiansi_description' })
 export class XianSi extends TriggerSkill {
@@ -121,23 +129,6 @@ export class XianSiShadow extends TriggerSkill implements OnDefineReleaseTiming 
   }
 }
 
-@ShadowSkill
-@CommonSkill({ name: XianSiShadow.Name, description: XianSi.Description })
-export class XianSiFilter extends GlobalFilterSkill {
-  canUseCardTo(cardId: CardId | CardMatcher, room: Room, owner: Player, attacker: Player, target: Player) {
-    if (!(cardId instanceof CardMatcher)) {
-      if (
-        VirtualCard.isVirtualCardId(cardId) &&
-        Sanguosha.getCardById<VirtualCard>(cardId).GeneratedBySkill === XianSiSlash.Name
-      ) {
-        return target.hasSkill(XianSi.Name);
-      }
-    }
-
-    return true;
-  }
-}
-
 @SideEffectSkill
 @CommonSkill({ name: XianSi.GeneralName, description: XianSi.Description })
 export class XianSiSlash extends ViewAsSkill {
@@ -187,17 +178,68 @@ export class XianSiSlash extends ViewAsSkill {
   cardFilter(room: Room, owner: Player, cards: CardId[]) {
     return cards.length === 0;
   }
+}
 
-  async beforeUse(room: Room, event: ServerEventFinder<GameEventIdentifiers.SkillUseEvent>) {
-    const { toIds, fromId } = event;
-    const to = room.getPlayerById(toIds![0]);
-    const niCards = to.getCardIds(PlayerCardsArea.OutsideArea, XianSi.GeneralName);
+@ShadowSkill
+@PersistentSkill()
+@CommonSkill({ name: XianSiSlash.Name, description: XianSiSlash.Description })
+export class XianSiSlashShadow extends TriggerSkill {
+  isAutoTrigger() {
+    return true;
+  }
+
+  get Muted() {
+    return true;
+  }
+
+  isTriggerable(
+    event: ServerEventFinder<GameEventIdentifiers.CardUseEvent | GameEventIdentifiers.CardResponseEvent>,
+    stage?: AllStage,
+  ) {
+    return (
+      (stage === CardUseStage.PreCardUse || stage === CardResponseStage.PreCardResponse) &&
+      Card.isVirtualCardId(event.cardId)
+    );
+  }
+
+  canUse(
+    room: Room,
+    owner: Player,
+    content: ServerEventFinder<GameEventIdentifiers.CardUseEvent | GameEventIdentifiers.CardResponseEvent>,
+  ) {
+    return Sanguosha.getCardById<VirtualCard>(content.cardId).findByGeneratedSkill(XianSi.Name);
+  }
+
+  async onTrigger(room: Room, event: ServerEventFinder<GameEventIdentifiers.SkillUseEvent>) {
+    event.translationsMessage = undefined;
+    return true;
+  }
+
+  async onEffect(room: Room, event: ServerEventFinder<GameEventIdentifiers.SkillUseEvent>) {
+    const actualEvent = event.triggeredOnEvent;
+    let fromId: PlayerId;
+    let toId: PlayerId;
+    if (EventPacker.getIdentifier(actualEvent!) === GameEventIdentifiers.CardUseEvent) {
+      fromId = (actualEvent as ServerEventFinder<GameEventIdentifiers.CardUseEvent>).fromId;
+      toId = TargetGroupUtil.getRealTargets(
+        (actualEvent as ServerEventFinder<GameEventIdentifiers.CardUseEvent>).targetGroup,
+      )[0];
+    } else if (EventPacker.getIdentifier(actualEvent!) === GameEventIdentifiers.CardResponseEvent) {
+      fromId = (actualEvent as ServerEventFinder<GameEventIdentifiers.CardUseEvent>).fromId;
+      toId = TargetGroupUtil.getRealTargets(
+        (actualEvent as ServerEventFinder<GameEventIdentifiers.CardUseEvent>).targetGroup,
+      )[0];
+    } else {
+      throw new Error('xiansi drop cards for slash failed');
+    }
+
+    const niCards = room.getPlayerById(toId).getCardIds(PlayerCardsArea.OutsideArea, XianSi.GeneralName);
 
     const askForChooseCardEvent: ServerEventFinder<GameEventIdentifiers.AskForChoosingCardEvent> = {
       toId: fromId,
       cardIds: niCards,
       amount: 2,
-      triggeredBySkills: [this.Name],
+      triggeredBySkills: [this.GeneralName],
     };
 
     room.notify(GameEventIdentifiers.AskForChoosingCardEvent, askForChooseCardEvent, fromId);
@@ -207,13 +249,11 @@ export class XianSiSlash extends ViewAsSkill {
       fromId,
     );
 
-    if (selectedCards === undefined) {
-      return false;
-    }
+    const actualCards = selectedCards ? selectedCards : niCards.slice(0, 2);
 
     await room.moveCards({
-      movingCards: selectedCards.map(card => ({ card, fromArea: CardMoveArea.HandArea })),
-      fromId: to.Id,
+      movingCards: actualCards.map(card => ({ card, fromArea: CardMoveArea.OutsideArea })),
+      fromId: toId,
       moveReason: CardMoveReason.PlaceToDropStack,
       toArea: CardMoveArea.DropStack,
       proposer: fromId,
