3138ab0500e56bbb6261d294c4495b249dde9475
Gaeric <1062217098@qq.com>
2022-08-01 22:36:21 +0800

    add new skill pve_longshen_zhifei

--- src/core/skills/game_mode/pve/pve_longshen_skills.ts
+++ src/core/skills/game_mode/pve/pve_longshen_skills.ts
@@ -1,13 +1,19 @@
-import { GameEventIdentifiers, ServerEventFinder } from 'core/event/event';
-import { AllStage, PhaseStageChangeStage, PlayerPhaseStages } from 'core/game/stage_processor';
+import { CardMoveArea, CardMoveReason, GameEventIdentifiers, ServerEventFinder } from 'core/event/event';
+import {
+  AllStage,
+  DrawCardStage,
+  PhaseStageChangeStage,
+  PlayerPhase,
+  PlayerPhaseStages,
+} from 'core/game/stage_processor';
 import { Player } from 'core/player/player';
 import { Room } from 'core/room/room';
 import { TriggerSkill } from 'core/skills/skill';
 import { CompulsorySkill } from 'core/skills/skill_wrappers';
 
-@CompulsorySkill({ name: 'pve_longshen_ziyu', description: 'pve_longshen_ziyu' })
+@CompulsorySkill({ name: 'pve_longshen_ziyu', description: 'pve_longshen_ziyu_description' })
 export class PveLongShenZiYu extends TriggerSkill {
-  isTriggerable(event: ServerEventFinder<GameEventIdentifiers.PhaseStageChangeEvent>, stage?: AllStage) {
+  isTriggerable(_: ServerEventFinder<GameEventIdentifiers.PhaseStageChangeEvent>, stage?: AllStage) {
     return stage === PhaseStageChangeStage.StageChanged;
   }
 
@@ -17,7 +23,7 @@ export class PveLongShenZiYu extends TriggerSkill {
     );
   }
 
-  async onTrigger(room: Room, content: ServerEventFinder<GameEventIdentifiers.SkillUseEvent>) {
+  async onTrigger() {
     return true;
   }
 
@@ -27,6 +33,40 @@ export class PveLongShenZiYu extends TriggerSkill {
       recoveredHp: owner.MaxHp - owner.Hp,
       recoverBy: owner.Id,
       toId: owner.Id,
+    });
+
+    return true;
+  }
+}
+
+@CompulsorySkill({ name: 'pve_longshen_zhifei', description: 'pve_longshen_zhifei_description' })
+export class PveLongShenZhiFei extends TriggerSkill {
+  isTriggerable(_: ServerEventFinder<GameEventIdentifiers.DrawCardEvent>, stage?: AllStage) {
+    return stage === DrawCardStage.BeforeDrawCardEffect;
+  }
+
+  canUse(room: Room, owner: Player, content: ServerEventFinder<GameEventIdentifiers.DrawCardEvent>) {
+    return (
+      content.fromId === room.CurrentPhasePlayer.Id &&
+      room.CurrentPlayerPhase === PlayerPhase.PlayCardStage &&
+      content.fromId !== owner.Id
+    );
+  }
+
+  async onTrigger() {
+    return true;
+  }
+
+  async onEffect(room: Room, content: ServerEventFinder<GameEventIdentifiers.SkillEffectEvent>) {
+    const target = room.CurrentPhasePlayer;
+    const card = target.getPlayerCards()[Math.floor(target.getPlayerCards().length * Math.random())];
+    await room.moveCards({
+      movingCards: [{ card }],
+      fromId: target.Id,
+      toId: content.fromId,
+      moveReason: CardMoveReason.ActivePrey,
+      toArea: CardMoveArea.HandArea,
+      movedByReason: this.Name,
     });
 
     return true;
