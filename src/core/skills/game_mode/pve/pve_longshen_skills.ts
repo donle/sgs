4441b27d0fe71dad5f691f6aee3589fb20b52b20
Gaeric <1062217098@qq.com>
2022-08-02 23:55:58 +0800

    pve skill pve_longshen_suwei

--- src/core/skills/game_mode/pve/pve_longshen_skills.ts
+++ src/core/skills/game_mode/pve/pve_longshen_skills.ts
@@ -1,5 +1,6 @@
-import { CardMoveArea, CardMoveReason, GameEventIdentifiers, ServerEventFinder } from 'core/event/event';
+import { CardMoveArea, CardMoveReason, EventPacker, GameEventIdentifiers, ServerEventFinder } from 'core/event/event';
 import {
+  AimStage,
   AllStage,
   DrawCardStage,
   PhaseStageChangeStage,
@@ -7,6 +8,7 @@ import {
   PlayerPhaseStages,
 } from 'core/game/stage_processor';
 import { Player } from 'core/player/player';
+import { PlayerCardsArea } from 'core/player/player_props';
 import { Room } from 'core/room/room';
 import { TriggerSkill } from 'core/skills/skill';
 import { CompulsorySkill } from 'core/skills/skill_wrappers';
@@ -69,6 +71,65 @@ export class PveLongShenChouXin extends TriggerSkill {
       movedByReason: this.Name,
     });
 
+    return true;
+  }
+}
+
+@CompulsorySkill({ name: 'pve_longshen_suwei', description: 'pve_longshen_suwei_description' })
+export class PveSuWei extends TriggerSkill {
+  isTriggerable(event: ServerEventFinder<GameEventIdentifiers.AimEvent>, stage?: AllStage) {
+    return stage === AimStage.AfterAimmed && event.byCardId !== undefined;
+  }
+
+  canUse(room: Room, owner: Player, event: ServerEventFinder<GameEventIdentifiers.AimEvent>) {
+    return event.toId === owner.Id && event.fromId !== owner.Id;
+  }
+
+  async onTrigger() {
+    return true;
+  }
+
+  async onEffect(room: Room, skillUseEvent: ServerEventFinder<GameEventIdentifiers.SkillUseEvent>) {
+    const { fromId, toId } = skillUseEvent.triggeredOnEvent as ServerEventFinder<GameEventIdentifiers.AimEvent>;
+    const attacker = room.getPlayerById(fromId);
+
+    await room.drawCards(1, toId, 'top', toId, this.Name);
+
+    if (room.getPlayerById(fromId).getPlayerCards().length > 0) {
+      const askForChooseCardEvent: ServerEventFinder<GameEventIdentifiers.AskForChoosingCardFromPlayerEvent> = {
+        options: {
+          [PlayerCardsArea.EquipArea]: attacker.getCardIds(PlayerCardsArea.EquipArea),
+          [PlayerCardsArea.HandArea]: attacker.getCardIds(PlayerCardsArea.HandArea).length,
+        },
+        fromId: toId,
+        toId: fromId,
+        triggeredBySkills: [this.Name],
+      };
+
+      room.notify(
+        GameEventIdentifiers.AskForChoosingCardFromPlayerEvent,
+        EventPacker.createUncancellableEvent<GameEventIdentifiers.AskForChoosingCardFromPlayerEvent>(
+          askForChooseCardEvent,
+        ),
+        toId,
+      );
+
+      const response = await room.onReceivingAsyncResponseFrom(
+        GameEventIdentifiers.AskForChoosingCardFromPlayerEvent,
+        toId,
+      );
+
+      if (response.selectedCardIndex !== undefined) {
+        const cardIds = attacker.getCardIds(PlayerCardsArea.HandArea);
+        response.selectedCard = cardIds[Math.floor(Math.random() * cardIds.length)];
+      } else if (response.selectedCard === undefined) {
+        const cardIds = attacker.getPlayerCards();
+        response.selectedCard = cardIds[Math.floor(Math.random() * cardIds.length)];
+      }
+      if (response.selectedCard !== undefined) {
+        await room.dropCards(CardMoveReason.PassiveDrop, [response.selectedCard], fromId, toId, this.Name);
+      }
+    }
     return true;
   }
 }
