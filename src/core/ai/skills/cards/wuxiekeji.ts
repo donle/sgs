7b7655b4eebd5e3a0b4e39623a93ddfe01fd1df9
donle <donle1988@gmail.com>
2022-09-29 10:12:21 +0800

    fix ai of wuxiekeji

--- src/core/ai/skills/cards/wuxiekeji.ts
+++ src/core/ai/skills/cards/wuxiekeji.ts
@@ -1,4 +1,5 @@
 import { AiLibrary } from 'core/ai/ai_lib';
+import { CardMatcher } from 'core/cards/libs/card_matcher';
 import type { CardId } from 'core/cards/libs/card_props';
 import type { ClientEventFinder, GameEventIdentifiers, ServerEventFinder } from 'core/event/event';
 import { Sanguosha } from 'core/game/engine';
@@ -11,22 +12,40 @@ export class WuXieKeJiSkillTrigger extends BaseSkillTrigger {
     room: Room,
     availableCards: CardId[],
   ): ClientEventFinder<GameEventIdentifiers.AskForCardUseEvent> | undefined {
-    const { scopedTargets, toId } = content;
-    const target = room.getPlayerById(scopedTargets![0]);
+    const { scopedTargets, toId, cardUserId } = content;
+    const target = scopedTargets?.[0] && room.getPlayerById(scopedTargets![0]);
     const ai = room.getPlayerById(toId);
 
-    const filteredAvailableCards = availableCards.filter(card => room.canUseCardTo(card, ai, target));
+    if (target) {
+      const filteredAvailableCards = availableCards.filter(card => room.canUseCardTo(card, ai, target));
 
-    if (filteredAvailableCards.length > 0) {
-      const card = content.byCardId != null && Sanguosha.getCardById(content.byCardId);
-      if (card && !AiLibrary.areTheyFriendly(ai, target, room.Info.gameMode)) {
-        return {
-          cardId: ['wugufengdeng', 'wuzhongshengyou', 'taoyuanjieyi'].includes(card.Name)
-            ? filteredAvailableCards[0]
-            : undefined,
-          fromId: ai.Id,
-        };
+      if (filteredAvailableCards.length > 0) {
+        const card = content.byCardId != null && Sanguosha.getCardById(content.byCardId);
+        if (card && !AiLibrary.areTheyFriendly(ai, target, room.Info.gameMode)) {
+          return {
+            cardId: ['wugufengdeng', 'wuzhongshengyou', 'taoyuanjieyi'].includes(card.Name)
+              ? filteredAvailableCards[0]
+              : undefined,
+            fromId: ai.Id,
+          };
+        }
       }
+    } else if (cardUserId) {
+      const filteredAvailableCards = availableCards.filter(card =>
+        ai.canUseCard(room, card, new CardMatcher(content.cardMatcher)),
+      );
+      return {
+        cardId: AiLibrary.areTheyFriendly(ai, room.getPlayerById(cardUserId), room.Info.gameMode)
+          ? undefined
+          : filteredAvailableCards.length > 0
+          ? filteredAvailableCards[0]
+          : undefined,
+        fromId: ai.Id,
+      };
     }
+
+    return {
+      fromId: ai.Id,
+    };
   }
 }
