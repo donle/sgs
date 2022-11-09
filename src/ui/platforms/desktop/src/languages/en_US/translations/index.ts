10398129446764516a6b63ba58c2f980aa8c4057
notify <notify-ctrl@qq.com>
2022-11-09 10:42:13 +0800

    fire

--- src/ui/platforms/desktop/src/languages/en_US/translations/index.ts
+++ src/ui/platforms/desktop/src/languages/en_US/translations/index.ts
@@ -1,6 +1,13 @@
 import * as cardsDictionary from './card';
+import * as fireCharactersDictionary from './fire';
 import * as generalDictionary from './general';
 import * as standardCharactersDictionary from './standard';
 import * as windCharactersDictionary from './wind';
 
-export { cardsDictionary, standardCharactersDictionary, generalDictionary, windCharactersDictionary };
+export {
+  cardsDictionary,
+  standardCharactersDictionary,
+  generalDictionary,
+  windCharactersDictionary,
+  fireCharactersDictionary,
+};
